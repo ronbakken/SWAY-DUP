c9049aa9e2f5c81a1de308c8b63011c5547ea1ac
Jan Boon <kaetemi@no-break.space>
2018-03-02 00:23:50 +0800

    Proper button interactivity

--- infx/infx/Widgets/RoundedButton.cs
+++ infx/infx/Widgets/RoundedButton.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using System.Text;
 
 using Xamarin.Forms;
+using Telerik.XamarinForms.Input;
 
 namespace infx
 {
-	public class RoundedButton : Button
+	public class RoundedButton : RadButton
 	{
 		public RoundedButton()
 		{
-			BackgroundColor = new Color(1.0, 1.0, 1.0, .0);
-			BorderColor = Palette.AccentColor;
-			TextColor = Palette.AccentColor;
+			BackgroundColor = new Color(1.0, 1.0, 1.0, 1.0);
+			BorderColor = Palette.Primary;
+			TextColor = Palette.Primary;
 			SizeChanged += RoundedButton_SizeChanged;
+			BorderWidth = Height * 0.05;
+			CornerRadius = (int)(Height * 0.5);
+			
 		}
 
 		private void RoundedButton_SizeChanged(object sender, EventArgs e)
 		{
-			BorderWidth = Height * 0.1;
+			BorderWidth = Height * 0.05;
+			CornerRadius = (int)(Height * 0.5);
 		}
 	}
 }
