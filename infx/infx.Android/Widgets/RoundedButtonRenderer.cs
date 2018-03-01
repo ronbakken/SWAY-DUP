6e09c01b5352cc807daee06fa0e07af2cd7e370d
Jan Boon <kaetemi@no-break.space>
2018-03-01 18:44:35 +0800

    Project fix

--- infx/infx.Android/Widgets/RoundedButtonRenderer.cs
+++ infx/infx.Android/Widgets/RoundedButtonRenderer.cs
@@ -59,7 +59,7 @@ namespace infx.Droid
 					var sld = new StateListDrawable();
 					sld.AddState(new int[] { Android.Resource.Attribute.StatePressed }, _pressed);
 					sld.AddState(new int[] { }, _normal);
-					Control.SetBackgroundDrawable(sld);
+					Control.SetBackground(sld);
 				};
 			}
 		}
