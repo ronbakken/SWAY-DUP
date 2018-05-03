f9184830ebcd7bfa80470baaa78f2eb244b99fee
Jan Boon <kaetemi@no-break.space>
2018-05-03 14:49:50 +0800

    Start rewrite

---
+++ flutter_home.bat
@@ -0,0 +1,8 @@
+
+title Flutter Console (INF)
+set PATH=C:\tools\flutter\bin;%PATH%
+set ANDROID_HOME=C:\tools\android-sdk\
+cd C:\source\infclient\infapp
+cls
+start "" "C:\Program Files\Microsoft VS Code\Code.exe"
+cmd
