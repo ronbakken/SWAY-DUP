23343d3689b2cad5080f5eee3af7f7e4c6849ba6
Jan Boon <kaetemi@no-break.space>
2018-05-04 16:42:41 +0800

    The slidey thing

--- infapp/lib/caroussel_app_bar.dart
+++ infapp/lib/caroussel_app_bar.dart
@@ -1,5 +1,57 @@
 import 'package:flutter/material.dart';
 
+class CarouselAppBar extends SliverAppBar {
+  CarouselAppBar({ Key key, BuildContext context, Widget title, List<String> imageUrls }) : 
+  super(
+    key: key,
+    pinned: true,
+      title: title,
+    expandedHeight: MediaQuery.of(context).size.width * 9.0 / 16.0,
+    flexibleSpace: new FlexibleSpaceBar(
+      background: _buildBackground(imageUrls),
+    ),
+  );
+
+  static Widget _buildBackground(List<String> imageUrls) {
+    print("Build Background");
+    List<Widget> images = new List<Widget>();
+    for (String imageUrl in imageUrls) {
+      images.add(new AspectRatio(
+        aspectRatio: 16.0 / 9.0,
+        child: new FadeInImage.assetNetwork(
+          placeholder: 'assets/placeholder_photo.png',
+          image: imageUrl,
+          fit: BoxFit.cover
+        ),
+      ));
+    }
+    return new AspectRatio(
+      aspectRatio: 16.0 / 9.0,
+      child: new Stack(
+        children: <Widget>[
+          images[0],
+          new AspectRatio(
+            aspectRatio: 16.0 / 4.0,
+            child: new ConstrainedBox(
+              constraints: new BoxConstraints.expand(),
+              child: new DecoratedBox(
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: <Color>[Colors.black45, Colors.transparent]
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
+
+/*
 class CarouselAppBar extends StatelessWidget {
   const CarouselAppBar({
     Key key,
@@ -33,9 +85,9 @@ class CarouselAppBar extends StatelessWidget {
     return new AspectRatio(
       aspectRatio: 16.0 / 9.0,
       child: new Stack(children: <Widget>[
-        /*images.length > 1 ? new TabBarView(
+        / *images.length > 1 ? new TabBarView(
           children: images,
-        ) :*/ images[0],
+        ) :* / images[0],
         new IgnorePointer(
           child: new Column(
             children: <Widget>[
@@ -69,4 +121,4 @@ class CarouselAppBar extends StatelessWidget {
       ]),
     );
   }
-}
+}*/
--- infapp/lib/offer_view.dart
+++ infapp/lib/offer_view.dart
@@ -6,73 +6,59 @@ import 'caroussel_app_bar.dart';
 class OfferView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListView(
-      children: [
-        new CarouselAppBar(
-          title: new Text("Fishing Season"),
-          imageUrls: [
-            'https://inf-dev.nyc3.digitaloceanspaces.com/demo/kahuna.jpg',
-            'https://inf-dev.nyc3.digitaloceanspaces.com/demo/friedfish.jpg',
-          ]
-        ),
-        /*new AspectRatio(
-          aspectRatio: 16.0 / 9.0,
-          child: new Stack(children: <Widget>[
-            new AspectRatio(
-              aspectRatio: 16.0 / 9.0,
-              child: new FadeInImage.assetNetwork(
-                placeholder: 'assets/placeholder_photo.png',
-                image: 'https://inf-dev.nyc3.digitaloceanspaces.com/demo/friedfish.jpg',
-                fit: BoxFit.cover
-              ),
-            ),
-            new Column(
-              children: <Widget>[
-                new AspectRatio(
-                  aspectRatio: 16.0 / 4.0,
-                  child: new ConstrainedBox(
-                    constraints: new BoxConstraints.expand(),
-                    child: new DecoratedBox(
-                      decoration: new BoxDecoration(
-                        gradient: new LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: <Color>[Colors.black87, Colors.transparent]
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],  
-            ),
-            new AppBar(
-              title: new Text("Fishing Season"),
-              backgroundColor: new Color.fromARGB(0, 0, 0, 0),
-              elevation: 0.0,
-            ),
-          ]),
-        ),*/
-        new ListTile(
-          leading: new CircleAvatar(
-            backgroundImage: new NetworkImage('https://inf-dev.nyc3.digitaloceanspaces.com/demo/kahuna.jpg')
+    return new Scaffold(
+      body: new CustomScrollView(
+        slivers: <Widget>[
+          new CarouselAppBar(
+            context: context,
+            title: new Text("Fishing Season"),
+            imageUrls: [
+              'https://inf-dev.nyc3.digitaloceanspaces.com/demo/kahuna.jpg',
+              'https://inf-dev.nyc3.digitaloceanspaces.com/demo/friedfish.jpg',
+            ]
           ),
-          title: new Text("Fried Willy"),
-          subtitle: new Text("1100 Glendon Avenue, 17th Floor, Los Angeles CA 90024"),
-        ),
-        new Divider(),
-        new ListTile(
-          title: new Text("Looking to catch more customers during the fishing season.", style: Theme.of(context).textTheme.body1 ),
-        ),
-        new ListTile(
-          leading: new Icon(Icons.work),
-          title: new Text("Posts with photography across social media.", style: Theme.of(context).textTheme.body1 ),
-        ),
-        new ListTile(
-          leading: new Icon(Icons.redeem),
-          title: new Text("Free dinner", style: Theme.of(context).textTheme.body1 ),
-        ),
-        new Divider(),
-      ]
+          new SliverList(
+            delegate: new SliverChildListDelegate([
+              new ListTile(
+                leading: new CircleAvatar(
+                  backgroundImage: new NetworkImage('https://inf-dev.nyc3.digitaloceanspaces.com/demo/kahuna.jpg')
+                ),
+                title: new Text("Fried Willy"),
+                subtitle: new Text("1100 Glendon Avenue, 17th Floor, Los Angeles CA 90024"),
+              ),
+              new Divider(),
+              new ListTile(
+                title: new Text("Looking to catch more customers during the fishing season.", style: Theme.of(context).textTheme.body1 ),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.work),
+                title: new Text("Posts with photography across social media.", style: Theme.of(context).textTheme.body1 ),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.redeem),
+                title: new Text("Free dinner", style: Theme.of(context).textTheme.body1 ),
+              ),
+              new Divider(),
+              new ListTile(
+                leading: new Icon(Icons.redeem),
+                title: new Text("Free dinner", style: Theme.of(context).textTheme.body1 ),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.redeem),
+                title: new Text("Free dinner", style: Theme.of(context).textTheme.body1 ),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.redeem),
+                title: new Text("Free dinner", style: Theme.of(context).textTheme.body1 ),
+              ),
+              new ListTile(
+                leading: new Icon(Icons.redeem),
+                title: new Text("Free dinner", style: Theme.of(context).textTheme.body1 ),
+              ),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
