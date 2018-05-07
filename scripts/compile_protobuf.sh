276d7f76734625020cd1370af0ebc1d17ce1695b
Jan Boon <kaetemi@no-break.space>
2018-05-07 12:52:36 +0800

    Update script

--- scripts/compile_protobuf.sh
+++ scripts/compile_protobuf.sh
@@ -9,3 +9,11 @@ git push
 git status
 cd ..
 cd scripts
+
+cd ../../infclient
+git pull
+cp ../infserver/protobuf/Inf.cs InfBuildConfig/Inf.cs
+git commit -m "Update protobuf"
+git push
+git status
+cd ../infserver/scripts
