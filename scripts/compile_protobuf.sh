f6f4a15e930d278328726485b4f200829e301e9c
Jan Boon <kaetemi@no-break.space>
2018-05-07 15:25:09 +0800

    Update script

--- scripts/compile_protobuf.sh
+++ scripts/compile_protobuf.sh
@@ -9,6 +9,12 @@ cp ../protobuf/*.dart lib/
 git add *
 cd ..
 git commit -m "Update protobuf"
+cd config
+pub get
+dart bin/main.dart
+git add *
+git commit -m "Update config"
+cd ..
 git push
 git status
 cd scripts
@@ -19,6 +25,9 @@ cp ../infserver/protobuf/Inf.cs InfBuildConfig/Inf.cs
 cp ../infserver/protobuf/*.dart infapp/lib/
 git add *
 git commit -m "Update protobuf"
+cp ../infserver/config/config.bin infapp/assets/config.bin
+git add *
+git commit -m "Update config"
 git push
 git status
 cd ../infserver/scripts
