811826a0bc05c0861fa6c3887994645f027786dd
Jan Boon <kaetemi@no-break.space>
2018-05-07 14:40:31 +0800

    Adjust script

--- scripts/compile_protobuf.sh
+++ scripts/compile_protobuf.sh
@@ -4,10 +4,13 @@ cd protobuf
 protoc --dart_out=. inf.proto
 protoc --csharp_out=. inf.proto
 git add *
+cd ../config
+cp ../infserver/protobuf/*.dart lib/
+git add *
+cd ..
 git commit -m "Update protobuf"
 git push
 git status
-cd ..
 cd scripts
 
 cd ../../infclient
