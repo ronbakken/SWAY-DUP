f113f6d94d0bac4adeba2118b83aa4169535b6d9
NO-BREAK SPACE OÜ <support@no-break.space>
2018-05-09 10:04:06 +0000

    Update protobuf

--- config/lib/inf.pbjson.dart
+++ config/lib/inf.pbjson.dart
@@ -18,11 +18,39 @@ const ConfigCategories$json = const {
   ],
 };
 
+const ConfigOAuthProvider$json = const {
+  '1': 'ConfigOAuthProvider',
+  '2': const [
+    const {'1': 'visible', '3': 1, '4': 1, '5': 8, '10': 'visible'},
+    const {'1': 'enabled', '3': 2, '4': 1, '5': 8, '10': 'enabled'},
+    const {'1': 'label', '3': 3, '4': 1, '5': 9, '10': 'label'},
+    const {'1': 'host', '3': 4, '4': 1, '5': 9, '10': 'host'},
+    const {'1': 'requestTokenUrl', '3': 5, '4': 1, '5': 9, '10': 'requestTokenUrl'},
+    const {'1': 'authenticateUrl', '3': 6, '4': 1, '5': 9, '10': 'authenticateUrl'},
+    const {'1': 'authUrl', '3': 7, '4': 1, '5': 9, '10': 'authUrl'},
+    const {'1': 'authQuery', '3': 8, '4': 1, '5': 9, '10': 'authQuery'},
+    const {'1': 'callbackUrl', '3': 9, '4': 1, '5': 9, '10': 'callbackUrl'},
+    const {'1': 'consumerKey', '3': 10, '4': 1, '5': 9, '10': 'consumerKey'},
+    const {'1': 'consumerSecret', '3': 11, '4': 1, '5': 9, '10': 'consumerSecret'},
+    const {'1': 'clientId', '3': 12, '4': 1, '5': 9, '10': 'clientId'},
+    const {'1': 'native', '3': 13, '4': 1, '5': 9, '10': 'native'},
+  ],
+};
+
+const ConfigOAuthProviders$json = const {
+  '1': 'ConfigOAuthProviders',
+  '2': const [
+    const {'1': 'all', '3': 1, '4': 3, '5': 11, '6': '.ConfigOAuthProvider', '10': 'all'},
+    const {'1': 'key', '3': 2, '4': 1, '5': 9, '10': 'key'},
+  ],
+};
+
 const Config$json = const {
   '1': 'Config',
   '2': const [
     const {'1': 'version', '3': 1, '4': 1, '5': 5, '10': 'version'},
     const {'1': 'categories', '3': 2, '4': 1, '5': 11, '6': '.ConfigCategories', '10': 'categories'},
+    const {'1': 'oauthProviders', '3': 3, '4': 1, '5': 11, '6': '.ConfigOAuthProviders', '10': 'oauthProviders'},
   ],
 };
 
--- protobuf/inf.pb.dart
+++ protobuf/inf.pb.dart
@@ -63,10 +63,146 @@ class ConfigCategories extends GeneratedMessage {
 
 class _ReadonlyConfigCategories extends ConfigCategories with ReadonlyMessageMixin {}
 
+class ConfigOAuthProvider extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('ConfigOAuthProvider')
+    ..aOB(1, 'visible')
+    ..aOB(2, 'enabled')
+    ..aOS(3, 'label')
+    ..aOS(4, 'host')
+    ..aOS(5, 'requestTokenUrl')
+    ..aOS(6, 'authenticateUrl')
+    ..aOS(7, 'authUrl')
+    ..aOS(8, 'authQuery')
+    ..aOS(9, 'callbackUrl')
+    ..aOS(10, 'consumerKey')
+    ..aOS(11, 'consumerSecret')
+    ..aOS(12, 'clientId')
+    ..aOS(13, 'native')
+    ..hasRequiredFields = false
+  ;
+
+  ConfigOAuthProvider() : super();
+  ConfigOAuthProvider.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  ConfigOAuthProvider.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  ConfigOAuthProvider clone() => new ConfigOAuthProvider()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static ConfigOAuthProvider create() => new ConfigOAuthProvider();
+  static PbList<ConfigOAuthProvider> createRepeated() => new PbList<ConfigOAuthProvider>();
+  static ConfigOAuthProvider getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyConfigOAuthProvider();
+    return _defaultInstance;
+  }
+  static ConfigOAuthProvider _defaultInstance;
+  static void $checkItem(ConfigOAuthProvider v) {
+    if (v is! ConfigOAuthProvider) checkItemFailed(v, 'ConfigOAuthProvider');
+  }
+
+  bool get visible => $_get(0, false);
+  set visible(bool v) { $_setBool(0, v); }
+  bool hasVisible() => $_has(0);
+  void clearVisible() => clearField(1);
+
+  bool get enabled => $_get(1, false);
+  set enabled(bool v) { $_setBool(1, v); }
+  bool hasEnabled() => $_has(1);
+  void clearEnabled() => clearField(2);
+
+  String get label => $_getS(2, '');
+  set label(String v) { $_setString(2, v); }
+  bool hasLabel() => $_has(2);
+  void clearLabel() => clearField(3);
+
+  String get host => $_getS(3, '');
+  set host(String v) { $_setString(3, v); }
+  bool hasHost() => $_has(3);
+  void clearHost() => clearField(4);
+
+  String get requestTokenUrl => $_getS(4, '');
+  set requestTokenUrl(String v) { $_setString(4, v); }
+  bool hasRequestTokenUrl() => $_has(4);
+  void clearRequestTokenUrl() => clearField(5);
+
+  String get authenticateUrl => $_getS(5, '');
+  set authenticateUrl(String v) { $_setString(5, v); }
+  bool hasAuthenticateUrl() => $_has(5);
+  void clearAuthenticateUrl() => clearField(6);
+
+  String get authUrl => $_getS(6, '');
+  set authUrl(String v) { $_setString(6, v); }
+  bool hasAuthUrl() => $_has(6);
+  void clearAuthUrl() => clearField(7);
+
+  String get authQuery => $_getS(7, '');
+  set authQuery(String v) { $_setString(7, v); }
+  bool hasAuthQuery() => $_has(7);
+  void clearAuthQuery() => clearField(8);
+
+  String get callbackUrl => $_getS(8, '');
+  set callbackUrl(String v) { $_setString(8, v); }
+  bool hasCallbackUrl() => $_has(8);
+  void clearCallbackUrl() => clearField(9);
+
+  String get consumerKey => $_getS(9, '');
+  set consumerKey(String v) { $_setString(9, v); }
+  bool hasConsumerKey() => $_has(9);
+  void clearConsumerKey() => clearField(10);
+
+  String get consumerSecret => $_getS(10, '');
+  set consumerSecret(String v) { $_setString(10, v); }
+  bool hasConsumerSecret() => $_has(10);
+  void clearConsumerSecret() => clearField(11);
+
+  String get clientId => $_getS(11, '');
+  set clientId(String v) { $_setString(11, v); }
+  bool hasClientId() => $_has(11);
+  void clearClientId() => clearField(12);
+
+  String get native => $_getS(12, '');
+  set native(String v) { $_setString(12, v); }
+  bool hasNative() => $_has(12);
+  void clearNative() => clearField(13);
+}
+
+class _ReadonlyConfigOAuthProvider extends ConfigOAuthProvider with ReadonlyMessageMixin {}
+
+class ConfigOAuthProviders extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('ConfigOAuthProviders')
+    ..pp<ConfigOAuthProvider>(1, 'all', PbFieldType.PM, ConfigOAuthProvider.$checkItem, ConfigOAuthProvider.create)
+    ..aOS(2, 'key')
+    ..hasRequiredFields = false
+  ;
+
+  ConfigOAuthProviders() : super();
+  ConfigOAuthProviders.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  ConfigOAuthProviders.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  ConfigOAuthProviders clone() => new ConfigOAuthProviders()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static ConfigOAuthProviders create() => new ConfigOAuthProviders();
+  static PbList<ConfigOAuthProviders> createRepeated() => new PbList<ConfigOAuthProviders>();
+  static ConfigOAuthProviders getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyConfigOAuthProviders();
+    return _defaultInstance;
+  }
+  static ConfigOAuthProviders _defaultInstance;
+  static void $checkItem(ConfigOAuthProviders v) {
+    if (v is! ConfigOAuthProviders) checkItemFailed(v, 'ConfigOAuthProviders');
+  }
+
+  List<ConfigOAuthProvider> get all => $_getList(0);
+
+  String get key => $_getS(1, '');
+  set key(String v) { $_setString(1, v); }
+  bool hasKey() => $_has(1);
+  void clearKey() => clearField(2);
+}
+
+class _ReadonlyConfigOAuthProviders extends ConfigOAuthProviders with ReadonlyMessageMixin {}
+
 class Config extends GeneratedMessage {
   static final BuilderInfo _i = new BuilderInfo('Config')
     ..a<int>(1, 'version', PbFieldType.O3)
     ..a<ConfigCategories>(2, 'categories', PbFieldType.OM, ConfigCategories.getDefault, ConfigCategories.create)
+    ..a<ConfigOAuthProviders>(3, 'oauthProviders', PbFieldType.OM, ConfigOAuthProviders.getDefault, ConfigOAuthProviders.create)
     ..hasRequiredFields = false
   ;
 
@@ -95,6 +231,11 @@ class Config extends GeneratedMessage {
   set categories(ConfigCategories v) { setField(2, v); }
   bool hasCategories() => $_has(1);
   void clearCategories() => clearField(2);
+
+  ConfigOAuthProviders get oauthProviders => $_getN(2);
+  set oauthProviders(ConfigOAuthProviders v) { setField(3, v); }
+  bool hasOauthProviders() => $_has(2);
+  void clearOauthProviders() => clearField(3);
 }
 
 class _ReadonlyConfig extends Config with ReadonlyMessageMixin {}
