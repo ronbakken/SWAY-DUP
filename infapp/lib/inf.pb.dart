955726a0b4c3c49990a7fefbf948b4aee2b157a6
NO-BREAK SPACE OÜ <support@no-break.space>
2018-05-10 06:04:33 +0000

    Update protobuf

--- infapp/lib/inf.pb.dart
+++ infapp/lib/inf.pb.dart
@@ -9,6 +9,8 @@ import 'dart:core' show int, bool, double, String, List, override;
 
 import 'package:protobuf/protobuf.dart';
 
+export 'inf.pbenum.dart';
+
 class ConfigSubCategories extends GeneratedMessage {
   static final BuilderInfo _i = new BuilderInfo('ConfigSubCategories')
     ..pPS(1, 'labels')
@@ -204,8 +206,8 @@ class ConfigOAuthProviders extends GeneratedMessage {
 
 class _ReadonlyConfigOAuthProviders extends ConfigOAuthProviders with ReadonlyMessageMixin {}
 
-class Config extends GeneratedMessage {
-  static final BuilderInfo _i = new BuilderInfo('Config')
+class ConfigData extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('ConfigData')
     ..a<int>(1, 'clientVersion', PbFieldType.O3)
     ..a<ConfigCategories>(2, 'categories', PbFieldType.OM, ConfigCategories.getDefault, ConfigCategories.create)
     ..a<ConfigOAuthProviders>(3, 'oauthProviders', PbFieldType.OM, ConfigOAuthProviders.getDefault, ConfigOAuthProviders.create)
@@ -213,20 +215,20 @@ class Config extends GeneratedMessage {
     ..hasRequiredFields = false
   ;
 
-  Config() : super();
-  Config.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
-  Config.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
-  Config clone() => new Config()..mergeFromMessage(this);
+  ConfigData() : super();
+  ConfigData.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  ConfigData.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  ConfigData clone() => new ConfigData()..mergeFromMessage(this);
   BuilderInfo get info_ => _i;
-  static Config create() => new Config();
-  static PbList<Config> createRepeated() => new PbList<Config>();
-  static Config getDefault() {
-    if (_defaultInstance == null) _defaultInstance = new _ReadonlyConfig();
+  static ConfigData create() => new ConfigData();
+  static PbList<ConfigData> createRepeated() => new PbList<ConfigData>();
+  static ConfigData getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyConfigData();
     return _defaultInstance;
   }
-  static Config _defaultInstance;
-  static void $checkItem(Config v) {
-    if (v is! Config) checkItemFailed(v, 'Config');
+  static ConfigData _defaultInstance;
+  static void $checkItem(ConfigData v) {
+    if (v is! ConfigData) checkItemFailed(v, 'ConfigData');
   }
 
   int get clientVersion => $_get(0, 0);
@@ -247,7 +249,7 @@ class Config extends GeneratedMessage {
   List<String> get downloadUrls => $_getList(3);
 }
 
-class _ReadonlyConfig extends Config with ReadonlyMessageMixin {}
+class _ReadonlyConfigData extends ConfigData with ReadonlyMessageMixin {}
 
 class CategoryId extends GeneratedMessage {
   static final BuilderInfo _i = new BuilderInfo('CategoryId')
@@ -311,4 +313,229 @@ class CategoryIdSet extends GeneratedMessage {
 }
 
 class _ReadonlyCategoryIdSet extends CategoryIdSet with ReadonlyMessageMixin {}
+
+class NetResRejected extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('NetResRejected')
+    ..aOS(1, 'rejectReason')
+    ..hasRequiredFields = false
+  ;
+
+  NetResRejected() : super();
+  NetResRejected.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  NetResRejected.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  NetResRejected clone() => new NetResRejected()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static NetResRejected create() => new NetResRejected();
+  static PbList<NetResRejected> createRepeated() => new PbList<NetResRejected>();
+  static NetResRejected getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyNetResRejected();
+    return _defaultInstance;
+  }
+  static NetResRejected _defaultInstance;
+  static void $checkItem(NetResRejected v) {
+    if (v is! NetResRejected) checkItemFailed(v, 'NetResRejected');
+  }
+
+  String get rejectReason => $_getS(0, '');
+  set rejectReason(String v) { $_setString(0, v); }
+  bool hasRejectReason() => $_has(0);
+  void clearRejectReason() => clearField(1);
+}
+
+class _ReadonlyNetResRejected extends NetResRejected with ReadonlyMessageMixin {}
+
+class NetReqImageUpload extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('NetReqImageUpload')
+    ..aOS(1, 'fileName')
+    ..a<int>(2, 'fileSize', PbFieldType.O3)
+    ..aOS(3, 'sha256')
+    ..hasRequiredFields = false
+  ;
+
+  NetReqImageUpload() : super();
+  NetReqImageUpload.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  NetReqImageUpload.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  NetReqImageUpload clone() => new NetReqImageUpload()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static NetReqImageUpload create() => new NetReqImageUpload();
+  static PbList<NetReqImageUpload> createRepeated() => new PbList<NetReqImageUpload>();
+  static NetReqImageUpload getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyNetReqImageUpload();
+    return _defaultInstance;
+  }
+  static NetReqImageUpload _defaultInstance;
+  static void $checkItem(NetReqImageUpload v) {
+    if (v is! NetReqImageUpload) checkItemFailed(v, 'NetReqImageUpload');
+  }
+
+  String get fileName => $_getS(0, '');
+  set fileName(String v) { $_setString(0, v); }
+  bool hasFileName() => $_has(0);
+  void clearFileName() => clearField(1);
+
+  int get fileSize => $_get(1, 0);
+  set fileSize(int v) { $_setUnsignedInt32(1, v); }
+  bool hasFileSize() => $_has(1);
+  void clearFileSize() => clearField(2);
+
+  String get sha256 => $_getS(2, '');
+  set sha256(String v) { $_setString(2, v); }
+  bool hasSha256() => $_has(2);
+  void clearSha256() => clearField(3);
+}
+
+class _ReadonlyNetReqImageUpload extends NetReqImageUpload with ReadonlyMessageMixin {}
+
+class NetResImageUpload extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('NetResImageUpload')
+    ..aOS(1, 'requestMethod')
+    ..aOS(2, 'requestUrl')
+    ..aOS(3, 'headerContentType')
+    ..aOS(4, 'headerContentLength')
+    ..aOS(5, 'headerHost')
+    ..aOS(6, 'headerXAmzDate')
+    ..aOS(7, 'headerXAmzStorageClass')
+    ..aOS(8, 'headerAuthorization')
+    ..hasRequiredFields = false
+  ;
+
+  NetResImageUpload() : super();
+  NetResImageUpload.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  NetResImageUpload.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  NetResImageUpload clone() => new NetResImageUpload()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static NetResImageUpload create() => new NetResImageUpload();
+  static PbList<NetResImageUpload> createRepeated() => new PbList<NetResImageUpload>();
+  static NetResImageUpload getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyNetResImageUpload();
+    return _defaultInstance;
+  }
+  static NetResImageUpload _defaultInstance;
+  static void $checkItem(NetResImageUpload v) {
+    if (v is! NetResImageUpload) checkItemFailed(v, 'NetResImageUpload');
+  }
+
+  String get requestMethod => $_getS(0, '');
+  set requestMethod(String v) { $_setString(0, v); }
+  bool hasRequestMethod() => $_has(0);
+  void clearRequestMethod() => clearField(1);
+
+  String get requestUrl => $_getS(1, '');
+  set requestUrl(String v) { $_setString(1, v); }
+  bool hasRequestUrl() => $_has(1);
+  void clearRequestUrl() => clearField(2);
+
+  String get headerContentType => $_getS(2, '');
+  set headerContentType(String v) { $_setString(2, v); }
+  bool hasHeaderContentType() => $_has(2);
+  void clearHeaderContentType() => clearField(3);
+
+  String get headerContentLength => $_getS(3, '');
+  set headerContentLength(String v) { $_setString(3, v); }
+  bool hasHeaderContentLength() => $_has(3);
+  void clearHeaderContentLength() => clearField(4);
+
+  String get headerHost => $_getS(4, '');
+  set headerHost(String v) { $_setString(4, v); }
+  bool hasHeaderHost() => $_has(4);
+  void clearHeaderHost() => clearField(5);
+
+  String get headerXAmzDate => $_getS(5, '');
+  set headerXAmzDate(String v) { $_setString(5, v); }
+  bool hasHeaderXAmzDate() => $_has(5);
+  void clearHeaderXAmzDate() => clearField(6);
+
+  String get headerXAmzStorageClass => $_getS(6, '');
+  set headerXAmzStorageClass(String v) { $_setString(6, v); }
+  bool hasHeaderXAmzStorageClass() => $_has(6);
+  void clearHeaderXAmzStorageClass() => clearField(7);
+
+  String get headerAuthorization => $_getS(7, '');
+  set headerAuthorization(String v) { $_setString(7, v); }
+  bool hasHeaderAuthorization() => $_has(7);
+  void clearHeaderAuthorization() => clearField(8);
+}
+
+class _ReadonlyNetResImageUpload extends NetResImageUpload with ReadonlyMessageMixin {}
+
+class NetReqCreateOffer extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('NetReqCreateOffer')
+    ..aOS(1, 'title')
+    ..pPS(2, 'imageIds')
+    ..aOS(3, 'description')
+    ..aOS(4, 'deliverables')
+    ..aOS(5, 'reward')
+    ..hasRequiredFields = false
+  ;
+
+  NetReqCreateOffer() : super();
+  NetReqCreateOffer.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  NetReqCreateOffer.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  NetReqCreateOffer clone() => new NetReqCreateOffer()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static NetReqCreateOffer create() => new NetReqCreateOffer();
+  static PbList<NetReqCreateOffer> createRepeated() => new PbList<NetReqCreateOffer>();
+  static NetReqCreateOffer getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyNetReqCreateOffer();
+    return _defaultInstance;
+  }
+  static NetReqCreateOffer _defaultInstance;
+  static void $checkItem(NetReqCreateOffer v) {
+    if (v is! NetReqCreateOffer) checkItemFailed(v, 'NetReqCreateOffer');
+  }
+
+  String get title => $_getS(0, '');
+  set title(String v) { $_setString(0, v); }
+  bool hasTitle() => $_has(0);
+  void clearTitle() => clearField(1);
+
+  List<String> get imageIds => $_getList(1);
+
+  String get description => $_getS(2, '');
+  set description(String v) { $_setString(2, v); }
+  bool hasDescription() => $_has(2);
+  void clearDescription() => clearField(3);
+
+  String get deliverables => $_getS(3, '');
+  set deliverables(String v) { $_setString(3, v); }
+  bool hasDeliverables() => $_has(3);
+  void clearDeliverables() => clearField(4);
+
+  String get reward => $_getS(4, '');
+  set reward(String v) { $_setString(4, v); }
+  bool hasReward() => $_has(4);
+  void clearReward() => clearField(5);
+}
+
+class _ReadonlyNetReqCreateOffer extends NetReqCreateOffer with ReadonlyMessageMixin {}
+
+class NetResCreateOffer extends GeneratedMessage {
+  static final BuilderInfo _i = new BuilderInfo('NetResCreateOffer')
+    ..a<int>(1, 'id', PbFieldType.O3)
+    ..hasRequiredFields = false
+  ;
+
+  NetResCreateOffer() : super();
+  NetResCreateOffer.fromBuffer(List<int> i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromBuffer(i, r);
+  NetResCreateOffer.fromJson(String i, [ExtensionRegistry r = ExtensionRegistry.EMPTY]) : super.fromJson(i, r);
+  NetResCreateOffer clone() => new NetResCreateOffer()..mergeFromMessage(this);
+  BuilderInfo get info_ => _i;
+  static NetResCreateOffer create() => new NetResCreateOffer();
+  static PbList<NetResCreateOffer> createRepeated() => new PbList<NetResCreateOffer>();
+  static NetResCreateOffer getDefault() {
+    if (_defaultInstance == null) _defaultInstance = new _ReadonlyNetResCreateOffer();
+    return _defaultInstance;
+  }
+  static NetResCreateOffer _defaultInstance;
+  static void $checkItem(NetResCreateOffer v) {
+    if (v is! NetResCreateOffer) checkItemFailed(v, 'NetResCreateOffer');
+  }
+
+  int get id => $_get(0, 0);
+  set id(int v) { $_setUnsignedInt32(0, v); }
+  bool hasId() => $_has(0);
+  void clearId() => clearField(1);
+}
+
+class _ReadonlyNetResCreateOffer extends NetResCreateOffer with ReadonlyMessageMixin {}
 
