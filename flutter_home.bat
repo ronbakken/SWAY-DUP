ee287c555925c681cd213db562b61e86d935f27a
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-06-15 17:03:44 +0800

    Update Config

--- flutter_home.bat
+++ flutter_home.bat
@@ -1,7 +1,7 @@
 
 title Flutter Console (INF)
 set PATH=C:\tools\flutter\bin;%PATH%
-set ANDROID_HOME=C:\tools\android-sdk\
+set ANDROID_HOME=C:\Users\Kenny\AppData\Local\Android\Sdk
 cd C:\source\infclient\infapp
 cls
 start "" "C:\Program Files\Microsoft VS Code\Code.exe"
