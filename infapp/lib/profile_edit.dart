ee194bbb76a31e4b7cb84c53b6b191adb56a5662
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-07-01 22:28:31 +0800

    Added Edit Profile Page

--- infapp/lib/profile_edit.dart
+++ infapp/lib/profile_edit.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class InfluencerDashboard extends StatelessWidget {
+class ProfileEdit extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Influencer Dashboard'),
+        title: new Text('Edit Profile'),
       ),
     );
   }
