741741d09603c0b8d89c566885ff04b2a350e60c
Jan Boon <kaetemi@no-break.space>
2018-07-23 20:54:52 +0800

    Moved to infcommon

--- scripts/light_compile_protobuf.sh
+++
@@ -1,3 +0,0 @@
-cd ~/infserver/protobuf
-protoc --dart_out=. inf.proto
-protoc --csharp_out=. inf.proto
--- scripts/saber_compile_protobuf.sh
+++
@@ -1,1 +0,0 @@
-ssh inf@saber.devinf.net 'PATH="$PATH:/usr/lib/dart/bin:$HOME/.pub-cache/bin" && cd infserver/scripts && git pull && ./compile_protobuf.sh'
