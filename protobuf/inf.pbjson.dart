e132c5ff9079413bd7fc54c6c2882c682eb9be39
Jan Boon <kaetemi@no-break.space>
2018-07-23 11:48:24 +0000

    Compile protobuf

--- protobuf/inf.pbjson.dart
+++ protobuf/inf.pbjson.dart
@@ -2,7 +2,6 @@
 //  Generated code. Do not modify.
 ///
 // ignore_for_file: non_constant_identifier_names,library_prefixes
-library inf_pbjson;
 
 const NetMessageType$json = const {
   '1': 'NetMessageType',
--- protobuf/inf.pbserver.dart
+++ protobuf/inf.pbserver.dart
@@ -2,7 +2,6 @@
 //  Generated code. Do not modify.
 ///
 // ignore_for_file: non_constant_identifier_names,library_prefixes
-library inf_pbserver;
 
 export 'inf.pb.dart';
 
