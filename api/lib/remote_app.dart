2c8a65f965bc2d4325ddeb91a99bf30a6465e62d
Jan Boon <kaetemi@no-break.space>
2018-07-28 08:08:06 +0800

    Load account

--- api/lib/remote_app.dart
+++ api/lib/remote_app.dart
@@ -11,6 +11,8 @@ import 'package:wstalk/wstalk.dart';
 import 'package:pointycastle/pointycastle.dart' as pointycastle;
 import 'package:pointycastle/block/aes_fast.dart' as pointycastle;
 
+// TODO: Move sql queries into a separate shared class, to allow prepared statements, and simplify code here
+
 class RemoteApp {
   bool _connected;
 
@@ -20,6 +22,14 @@ class RemoteApp {
   final random = new Random.secure();
 
   int deviceId;
+  AccountType accountType;
+  int accountId;
+
+  GlobalAccountState globalAccountState;
+  GlobalAccountStateReason globalAccountStateReason;
+  int addressId;
+
+  List<DataSocialMedia> socialMedia = new List<DataSocialMedia>(10); // TODO: Proper length from configuration
 
   RemoteApp(this.sql, this.ts) {
     subscribeAuthentication();
@@ -90,10 +100,13 @@ class RemoteApp {
       Future<TalkMessage> signatureMessageFuture = ts.sendRequest(_netDeviceAuthChallengeResReq, challengePb.writeToBuffer(), reply: message);
       
       // Get the pub_key from the device that can be used to decrypt the signed challenge
-      sqljocky.Results pubKeyResults = await sql.prepareExecute("SELECT `aes_key` FROM `devices` WHERE `deviceId` = ?", [ attemptDeviceId ]);
+      sqljocky.Results pubKeyResults = await sql.prepareExecute("SELECT `aes_key` FROM `devices` WHERE `device_id` = ?", [ attemptDeviceId ]);
       Uint8List aesKey;// = base64.decode(input)
       await for (sqljocky.Row row in pubKeyResults) {
         aesKey = base64.decode(row[0].toString());
+      }
+      if (aesKey.length == 0) {
+        print("AES key missing for device $attemptDeviceId");
       }
 
       // Await signature
@@ -123,13 +136,58 @@ class RemoteApp {
         } else {
           print("Signature verification failed for device $attemptDeviceId");
         }
+      } else if (signaturePb.signature.length == 0) {
+        print("Signature missing from authentication message for device $attemptDeviceId");
       }
 
       // Send authentication state
-      sendNetDeviceAuthState(reply: message);
+      await updateDeviceState();
+      if (accountId != 0) {
+        unsubscribeAuthentication(); // No longer respond to authentication messages when OK
+      }
+      sendNetDeviceAuthState(reply: signatureMessage);
     } catch (ex) {
       print("Exception in message '${TalkSocket.decode(message.id)}':");
       print(ex);
+    }
+  }
+
+  Future updateDeviceState() async {
+    // First get the account id (and account type, in case the account id has not been created yet)
+    sqljocky.Results deviceResults = await sql.prepareExecute("SELECT `account_id`, `account_type` FROM `devices` WHERE `device_id` = ?", [ deviceId ]);
+    await for (sqljocky.Row row in deviceResults) { // one row
+      accountId = row[0]; // VERIFY
+      accountType = AccountType.valueOf(row[1]); // VERIFY
+    }
+    sqljocky.Results mediaResults;
+    if (accountId != 0) {
+      // Get the account information if it exists
+      sqljocky.Results accountResults = await sql.prepareExecute("SELECT `account_type`, `global_account_state`, `global_account_state_reason`, `address_id` FROM `accounts` WHERE `account_id` = ?", [ accountId ]);
+      await for (sqljocky.Row row in accountResults) { // one row
+        accountType = AccountType.valueOf(row[0]); // VERIFY
+        globalAccountState = GlobalAccountState.valueOf(row[1]); // VERIFY
+        globalAccountStateReason = GlobalAccountStateReason.valueOf(row[2]); // VERIFY
+        addressId = row[3]; // VERIFY
+      }
+      mediaResults = await sql.prepareExecute("SELECT `oauth_provider`, `username`, `display_name`, `followers`, `following` FROM `oauth_connections` WHERE `account_id` = ?", [ accountId ]);
+    } else {
+      mediaResults = await sql.prepareExecute("SELECT `oauth_provider`, `username`, `display_name`, `followers`, `following` FROM `oauth_connections` WHERE `device_id` = ?", [ deviceId ]);
+    }
+    List<bool> connected = new List<bool>(socialMedia.length);
+    await for (sqljocky.Row row in mediaResults) {
+      int oauthProvider = row[0];
+      if (oauthProvider < socialMedia.length) {
+        connected[oauthProvider] = true;
+        socialMedia[oauthProvider].userName = row[1].toString();
+        socialMedia[oauthProvider].displayName = row[2].toString();
+        socialMedia[oauthProvider].followers = row[3];
+        socialMedia[oauthProvider].following = row[4];
+      } else {
+        print("Unknown social media provider $oauthProvider");
+      }
+    }
+    for (int i = 0; i < socialMedia.length; ++i) {
+      socialMedia[i].connected = connected[i];
     }
   }
 
@@ -140,6 +198,11 @@ class RemoteApp {
     }
     NetDeviceAuthState pb = new NetDeviceAuthState();
     pb.deviceId = deviceId;
+    pb.accountId = accountId;
+    pb.accountType = accountType;
+    pb.globalAccountState = globalAccountState;
+    pb.globalAccountStateReason = globalAccountStateReason;
+    pb.socialMedia.setAll(0, socialMedia);
     ts.sendMessage(_netDeviceAuthState, pb.writeToBuffer(), reply: reply);
   }
 
