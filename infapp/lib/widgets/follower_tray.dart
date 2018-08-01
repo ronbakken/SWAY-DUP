95ad12a457f6391db14a7384e5f79add5e24a79e
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-01 21:04:46 +0800

    Updated Follower Widgets

--- infapp/lib/widgets/follower_tray.dart
+++ infapp/lib/widgets/follower_tray.dart
@@ -9,18 +9,25 @@ class FollowerTray extends StatelessWidget
   FollowerTray({
     Key key, 
     this.oAuthProviders,
+    this.socialMedia,
     }) :super (key: key);
 
   final List<ConfigOAuthProvider> oAuthProviders;
+  final List<DataSocialMedia> socialMedia;
 
   @override
   Widget build(BuildContext context){
 
     List<FollowerWidget> followerWidgets = new List<FollowerWidget>();
     
-    for (int i = 1; i < oAuthProviders.length; i++) {
-      if(oAuthProviders[i].enabled) {
-        followerWidgets.add(new FollowerWidget(oAuthProvider: oAuthProviders[i]));
+    for (int i = 1; i < socialMedia.length; i++) {
+      if(socialMedia[i].connected) {
+        followerWidgets.add(
+          new FollowerWidget(
+            oAuthProvider: oAuthProviders[i], 
+            followerCount: socialMedia[i].followersCount,
+          )
+        );
       }
     }
 
