11f4b5513061f5d451263c2a2d228a4efedcb5e4
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-02 06:25:22 +0800

    Added Search Page

---
+++ infapp/lib/search/search_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatelessWidget {
+
+  SearchPage({
+    Key key,
+  }) : super(key: key);
+
+	@override
+  Widget build(BuildContext context) 
+  {
+    return new Scaffold();
+  }
+}
