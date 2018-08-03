dc60778ec7619a5988dd7cd4f7cb97523a840f36
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-03 10:38:42 +0800

    Changed Profile Edit to Stateful Widget

--- infapp/lib/profile/profile_edit.dart
+++ infapp/lib/profile/profile_edit.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
-class ProfileEdit extends StatelessWidget {
+class ProfileEdit extends StatefulWidget 
+{
+  
+  @override
+  _ProfileEditState createState() => new _ProfileEditState();
+}
+
+class _ProfileEditState extends State<ProfileEdit>
+{
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Edit Profile'),
+      ),
+      body: new ListView( 
+        padding: const EdgeInsets.all(8.0),
+        children: <Widget>[
+
+        ],
       ),
     );
   }
