0689ada4bd69c0c2e2275a16727d7d62316805ab
Jan Boon <kaetemi@no-break.space>
2018-08-08 14:28:10 +0800

    Upload script

--- config/bin/upload.dart
+++ config/bin/upload.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
-import 'dart:async';
 import 'package:config/inf.pb.dart';
-import 'package:dospace/dospace.dart';
+import 'package:dospace/dospace.dart' as dospace;
 
 main() async {
   ConfigData serverConfig = new ConfigData();
-  // new File("config_server.bin")
+  serverConfig.mergeFromBuffer(await new File("config_server.bin").readAsBytes());
+  var spaces = new dospace.Spaces(
+    region: serverConfig.services.spacesRegion,
+    accessKey: serverConfig.services.spacesKey,
+    secretKey: serverConfig.services.spacesSecret,
+  );
+  var bucket = spaces.bucket(serverConfig.services.spacesBucket);
+  String etag = await bucket.uploadFile('config/config.bin', 'config.bin', 'application/octet-stream', dospace.Permissions.Public);
+  print(etag);
 }
 
 /* end of file */
