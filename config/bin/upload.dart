ac6329068de75d7dcc07e3457a04fd3b44541138
Jan Boon <kaetemi@no-break.space>
2018-08-08 06:17:48 +0000

    Upload scripts

---
+++ config/bin/upload.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:config/inf.pb.dart';
+import 'package:dospace/dospace.dart';
+
+main() async {
+  ConfigData serverConfig = new ConfigData();
+  // new File("config_server.bin")
+}
+
+/* end of file */
