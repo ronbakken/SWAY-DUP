fe80b3e7d9d8ff83e92d0b096e1fdec75e4a25a7
Jan Boon <kaetemi@no-break.space>
2018-08-09 08:39:06 +0000

    Update protobuf

--- config/bin/main.dart
+++ config/bin/main.dart
@@ -165,6 +165,7 @@ Future<ConfigServices> generateConfigServices(bool server) async {
       if (cfg.hasOption(section, 'spacesSecret')) res.spacesSecret = cfg.get(section, 'spacesSecret');
       if (cfg.hasOption(section, 'spacesBucket')) res.spacesBucket = cfg.get(section, 'spacesBucket');
       
+      if (cfg.hasOption(section, 'ipstackApi')) res.ipstackApi = cfg.get(section, 'ipstackApi');
       if (cfg.hasOption(section, 'ipstackKey')) res.ipstackKey = cfg.get(section, 'ipstackKey');
     }
   }
