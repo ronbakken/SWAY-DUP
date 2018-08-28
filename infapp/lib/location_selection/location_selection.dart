5805ce8bbd0772cfc9e2a8738b5cfde6af33d062
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-29 06:56:17 +0800

    Initial location_selection

---
+++ infapp/lib/location_selection/location_selection.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LocationSelectionScreen extends StatefulWidget
+{
+  @override
+  _LocationSelectionState createState() => new _LocationSelectionState();
+}
+
+class _LocationSelectionState extends State<LocationSelectionScreen>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return new Scaffold();
+  }
+}
