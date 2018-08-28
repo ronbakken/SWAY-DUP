ee519617e0320df7cebb3fdb788440ea774029da
NO-BREAK SPACE OÜ <support@no-break.space>
2018-08-28 12:15:54 +0000

    Dartfmt

--- infapp/lib/network/network_manager.dart
+++ infapp/lib/network/network_manager.dart
@@ -666,7 +666,8 @@ class _NetworkManagerState extends State<_NetworkManagerStateful>
   DataAccount latestAccount(DataAccount account) {
     // Check any caches if we have, otherwise just return
     // TODO: Timestamps...
-    if (account.state.accountId == this.account.state.accountId) { // It's me...
+    if (account.state.accountId == this.account.state.accountId) {
+      // It's me...
       return this.account;
     }
     return account;
@@ -677,7 +678,8 @@ class _NetworkManagerState extends State<_NetworkManagerStateful>
   DataBusinessOffer latestBusinessOffer(DataBusinessOffer offer) {
     // Check any caches if we have, otherwise just return
     // TODO: Timestamps...
-    if (_offers.containsKey(offer.offerId)) { // Guaranteed to be the latest
+    if (_offers.containsKey(offer.offerId)) {
+      // Guaranteed to be the latest
       return _offers[offer.offerId];
     }
     return offer;
