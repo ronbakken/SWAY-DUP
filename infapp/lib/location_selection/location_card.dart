2ef23e67e7bae5c8e2bb3178f6be9b553b56e088
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-29 20:57:27 +0800

    Added Location card for location_search

---
+++ infapp/lib/location_selection/location_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LocationCard extends StatelessWidget {
+  LocationCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card();
+  }
+}
