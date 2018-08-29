04b3c1cd62869c7c45cd909804f3b6c24e7452ef
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-29 17:31:11 +0800

    Added Location Search Screen

---
+++ infapp/lib/location_selection/location_search.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LocationSearchScreen extends StatefulWidget {
+  LocationSearchScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _LocationSearchPageState createState() => new _LocationSearchPageState();
+}
+
+class _LocationSearchPageState extends State<LocationSearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
