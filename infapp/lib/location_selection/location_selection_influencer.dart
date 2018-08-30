c5343f7517c4d291f524795467a2f5016d800538
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-30 08:29:29 +0800

    Added location selection for influencer

---
+++ infapp/lib/location_selection/location_selection_influencer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LocationInfluencerScreen extends StatefulWidget
+{
+  LocationInfluencerScreen({
+    Key key,
+  }) : super (key: key);
+
+  @override
+  _LocationInfluencerState createState() => new _LocationInfluencerState();
+}
+
+
+class _LocationInfluencerState extends State<LocationInfluencerScreen>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return new Scaffold();
+  }
+}
