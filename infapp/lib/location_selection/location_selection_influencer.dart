30b5f180cc8fa6b9f926bae1bc2ea5e1973ac41f
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-08-30 10:01:47 +0800

    set up widgets for influencer location

--- infapp/lib/location_selection/location_selection_influencer.dart
+++ infapp/lib/location_selection/location_selection_influencer.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
+/// Location Selection Screen that will find 
+/// the influencer profile's location.
 class LocationInfluencerScreen extends StatefulWidget
 {
   LocationInfluencerScreen({
     Key key,
   }) : super (key: key);
 
+  VoidCallback onSearch;
+  VoidCallback onConfirm;
+  
   @override
   _LocationInfluencerState createState() => new _LocationInfluencerState();
 }
@@ -13,6 +19,14 @@ class LocationInfluencerScreen extends StatefulWidget
 
 class _LocationInfluencerState extends State<LocationInfluencerScreen>
 {
+  FloatingActionButton confirmButton;
+  Marker marker;
+  TextField search;
+  TextEditingController _searchController;
+  IconButton searchButton;
+  MapController _mapController;
+  FlutterMap map;
+
   @override
   Widget build(BuildContext context)
   {
