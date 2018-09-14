b61dccd972a5f88a7487ea94610148c7bd073d10
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-14 20:17:23 +0800

    get position based on input coordinates

--- infapp/lib/location_selection/experiment_files/coordinates_to_address.dart
+++ infapp/lib/location_selection/experiment_files/coordinates_to_address.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-class LookupCoordinatesWidget extends StatefulWidget {
+class CoordinatesToAddress extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _LookupCoordinatesState();
+  State<StatefulWidget> createState() => _CoordinatesToAddressState();
 }
 
-class _LookupCoordinatesState extends State<LookupCoordinatesWidget> {
+class _CoordinatesToAddressState extends State<CoordinatesToAddress> {
   Geolocator _geolocator = new Geolocator();
   TextEditingController _coordinatesTextController = new TextEditingController();
   String _placemark = "";
 
+  void _onLookupAddressPressed() async {
+    List<String> coords = _coordinatesTextController.text.split(',');
+    double latitude = double.parse(coords[0]);
+    double longitude = double.parse(coords[1]);
+
+    List<Placemark> placemarks =
+        await _geolocator.placemarkFromCoordinates(latitude, longitude);
+
+      Placemark position = placemarks[0];      
+      _placemark = position.thoroughfare + " " + position.locality;
+      
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return Scaffold(
+      
+    );
   }
 }
