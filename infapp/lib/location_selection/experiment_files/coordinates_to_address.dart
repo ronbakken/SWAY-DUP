3b32144f224a0b8cc754f95afec0c06e9695c19e
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-14 18:14:38 +0800

    Added pratice files for geolocator

---
+++ infapp/lib/location_selection/experiment_files/coordinates_to_address.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LookupCoordinatesWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LookupCoordinatesState();
+}
+
+class _LookupCoordinatesState extends State<LookupCoordinatesWidget> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
