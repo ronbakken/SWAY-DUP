0ee7aa889c50f0ecde9cea784f336bfa87d6ac55
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-15 10:27:06 +0800

    look up coordinates from address

--- infapp/lib/location_selection/experiment_files/address_to_coordinates.dart
+++ infapp/lib/location_selection/experiment_files/address_to_coordinates.dart
@@ -11,7 +11,20 @@ class _AddressToCoordinatesState extends State<AddressToCoordinates> {
   Geolocator _geolocator = new Geolocator();
   TextEditingController _addressTextController = new TextEditingController();
 
-   LatLng coords = new LatLng(0.00, 0.00);
+  LatLng coords = new LatLng(0.00, 0.00);
+
+  void _onLookupCoordinatesPressed() async {
+    List<Placemark> placemarks =
+        await _geolocator.placemarkFromAddress(_addressTextController.text);
+
+    if (placemarks != null && placemarks.length >= 1) {
+      Placemark pos = placemarks[0];
+      setState(() {
+        coords.latitude = pos.position.latitude;
+        coords.longitude = pos.position.longitude;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
