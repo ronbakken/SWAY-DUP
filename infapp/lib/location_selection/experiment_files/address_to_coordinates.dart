c5cd2ffccc79505fec2e1a217038dda3dfa44917
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-15 08:49:44 +0800

    Added latlong to addresstocoords

--- infapp/lib/location_selection/experiment_files/address_to_coordinates.dart
+++ infapp/lib/location_selection/experiment_files/address_to_coordinates.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:latlong/latlong.dart';
 
 class AddressToCoordinates extends StatefulWidget {
   @override
@@ -7,7 +8,10 @@ class AddressToCoordinates extends StatefulWidget {
 }
 
 class _AddressToCoordinatesState extends State<AddressToCoordinates> {
-  
+  Geolocator _geolocator = new Geolocator();
+  TextEditingController _addressTextController = new TextEditingController();
+
+   LatLng coords = new LatLng(0.00, 0.00);
 
   @override
   Widget build(BuildContext context) {
