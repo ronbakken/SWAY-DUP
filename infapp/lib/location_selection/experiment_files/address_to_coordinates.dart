1666e7f1470aeffe431831aa3ad9a374a3cdcdbe
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-15 08:04:47 +0800

    Added address to coordinates

---
+++ infapp/lib/location_selection/experiment_files/address_to_coordinates.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class AddressToCoordinates extends StatefulWidget {
+  @override
+  _AddressToCoordinatesState createState() => _AddressToCoordinatesState();
+}
+
+class _AddressToCoordinatesState extends State<AddressToCoordinates> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
