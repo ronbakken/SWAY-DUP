55f033ebfc973ff974079819c7dead192e97b59e
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-21 19:45:06 +0800

    Check if request was successful

--- infapp/lib/location_selection/experiment_files/reverse_geocoding.dart
+++ infapp/lib/location_selection/experiment_files/reverse_geocoding.dart
@@ -1,22 +1,27 @@
 import 'dart:async';
-import 'package:http/http.dart';
-import 'dart:io';
 import 'dart:convert';
 
+import 'package:http/http.dart';
+
+import '../../network/inf.pb.dart';
+
 Future<String> coordinatesToAddress(double latitude, double longitude) async {
-  // Mapbox endpoint for forward and reverse geocoding
+  // Mapbox endpoint for reverse geocoding
   // /geocoding/v5/{mode}/{longitude},{latitude}.json
-  // /geocoding/v5/{mode}/{query}.json
   String url = "https://api.mapbox.com/geocoding/v5/"
       "mapbox.places/$longitude,$latitude.json?"
       "access_token=pk.eyJ1IjoibmJzcG91IiwiYSI6ImNqazRwN3h4ODBjM2QzcHA2N2ZzbHoyYm0ifQ.vpwrdXRoCU-nBm-E1KNKdA";
 
-  // Http Request for Geocoding
-  Request request = new Request("GET", Uri.dataFromString(url));
-
   // Get Response from Geocode Request
   Response response = await get(url);
 
-  print(response.body);
+  if (response.statusCode == 200) {
+    // If the request was successful, parse the JSON
+    
+  } else {
+    // If that call was not successful, throw an error.
+    throw Exception('Failed to retrieve location');
+  }
 
+  print(response.body);
 }
