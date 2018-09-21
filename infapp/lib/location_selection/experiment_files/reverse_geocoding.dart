b7dcee467819a86eea81f89cff97a7d28e7f17ce
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2018-09-21 20:35:19 +0800

    Get place_name from coordinates

--- infapp/lib/location_selection/experiment_files/reverse_geocoding.dart
+++ infapp/lib/location_selection/experiment_files/reverse_geocoding.dart
@@ -12,16 +12,27 @@ Future<String> coordinatesToAddress(double latitude, double longitude) async {
       "mapbox.places/$longitude,$latitude.json?"
       "access_token=pk.eyJ1IjoibmJzcG91IiwiYSI6ImNqazRwN3h4ODBjM2QzcHA2N2ZzbHoyYm0ifQ.vpwrdXRoCU-nBm-E1KNKdA";
 
-  // Get Response from Geocode Request
+  // Get Response from Geocode Request 'GET'
   Response response = await get(url);
 
+  // Check if connection is successful
   if (response.statusCode == 200) {
     // If the request was successful, parse the JSON
-    
+    print('Success!');
   } else {
     // If that call was not successful, throw an error.
     throw Exception('Failed to retrieve location');
   }
 
-  print(response.body);
+  // Decode info from source
+  dynamic doc = json.decode(response.body);
+
+  // Get features from info
+  dynamic features = doc['features'];
+
+  for (dynamic feature in features) {
+    dynamic placeName = feature['place_name'];
+
+    print(placeName);
+  }
 }
