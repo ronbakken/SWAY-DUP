86e39de1df978fb69833b8cc874588898b1d52d6
Kenneth Amiel Santos <kennethamiel.santos@benilde.edu.ph>
2018-09-23 00:39:38 +0800

    Implement buildAction for LocationSearch

--- infapp/lib/location_selection/location_search.dart
+++ infapp/lib/location_selection/location_search.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 
-class LocationSearch extends SearchDelegate<String>
-{
+class LocationSearch extends SearchDelegate<String> {
+  // Actions for Appbar
   @override
-  List<Widget> buildActions(BuildContext context)
-  {
-    // TODO: Implement Clear Search Query
+  List<Widget> buildActions(BuildContext context) {
+    // Implement Clear Search Query
+    return <Widget>[
+      new IconButton(
+        icon: new Icon(Icons.clear),
+        onPressed: () {
+          // Clear the query as the user presses the clear button
+          // NOTE: query is a property of the parent class
+          query = "";
+        },
+      ),
+    ];
   }
 
+  // Icon on the left of the Appbar
   @override
-  Widget buildLeading(BuildContext context)
-  {
+  Widget buildLeading(BuildContext context) {
     // TODO: Implement Leading Back Icon
   }
 
+  // Action when a List item from search in selected
   @override
-  Widget buildResults(BuildContext context)
-  {
+  Widget buildResults(BuildContext context) {
     // TODO: Implement callback when result is tapped
   }
 
+  // List of items to be shown when typing a query
   @override
-  Widget buildSuggestions(BuildContext context)
-  {
+  Widget buildSuggestions(BuildContext context) {
     // TODO: Implement Location List
-  } 
+  }
 }
-
