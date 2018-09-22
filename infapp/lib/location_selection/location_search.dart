b9737e8753a2bd2ad7574163651e29172b2858a6
Kenneth Amiel Santos <kennethamiel.santos@benilde.edu.ph>
2018-09-22 22:52:42 +0800

    Notes

--- infapp/lib/location_selection/location_search.dart
+++ infapp/lib/location_selection/location_search.dart
@@ -5,25 +5,25 @@ class LocationSearch extends SearchDelegate<String>
   @override
   List<Widget> buildActions(BuildContext context)
   {
-
+    // TODO: Implement Clear Search Query
   }
 
   @override
   Widget buildLeading(BuildContext context)
   {
-    
+    // TODO: Implement Leading Back Icon
   }
 
   @override
   Widget buildResults(BuildContext context)
   {
-    
+    // TODO: Implement callback when result is tapped
   }
 
   @override
   Widget buildSuggestions(BuildContext context)
   {
-    
+    // TODO: Implement Location List
   } 
 }
 
