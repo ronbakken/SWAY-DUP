f99c0a7eb4905932dc52601de2f55221de62ec5b
Kenneth Amiel Santos <kennethamiel.santos@benilde.edu.ph>
2018-09-24 11:00:19 +0800

    Added mapbox endpoint in converting to coordinates

--- infapp/lib/location_selection/experiment_files/lookup_address.dart
+++ infapp/lib/location_selection/experiment_files/lookup_address.dart
@@ -4,6 +4,11 @@ import 'package:latlong/latlong.dart';
 import 'package:http/http.dart';
 
 Future<LatLng> getCoordinates(String address) async {
-  
+  // Mapbox endpoint for reverse geocoding
+  // /geocoding/v5/{mode}/{address}.json
+  String url = "https://api.mapbox.com/geocoding/v5/"
+      "mapbox.places/$address.json?"
+      "autocomplete=true&"
+      "access_token=pk.eyJ1IjoibmJzcG91IiwiYSI6ImNqazRwN3h4ODBjM2QzcHA2N2ZzbHoyYm0ifQ.vpwrdXRoCU-nBm-E1KNKdA";
  
 }
