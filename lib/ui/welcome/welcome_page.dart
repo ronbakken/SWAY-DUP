567af22becb2348ac655b80b385144c676ee715e
Thomas Burkhart <github@burkharts.net>
2018-11-01 19:05:07 +0100

    working

--- lib/ui/welcome/welcome_page.dart
+++ lib/ui/welcome/welcome_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:inf/backend/backend.dart';
+import 'package:inf/domain/domain.dart';
+import 'package:inf/ui/welcome/onboarding_business.dart';
+import 'package:inf/ui/welcome/onboarding_inf.dart';
+import 'package:inf/ui/widgets/navigation_functions.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Welcome'),
-      
+    return Material(
+      child: Stack(
+        children: <Widget>[
+          Placeholder(),
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Text('Welcome'),
+                ),
+              ),
+              FlatButton(
+                onPressed: () async {
+                  await backend.get<AuthenticationService>().setUserType(UserType.influcencer);
+                  await replacePage(context, OnboardingPageInfluencer());
+                },
+                child: Text('Influencer'),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              FlatButton(
+                onPressed: () async {
+                  await backend.get<AuthenticationService>().setUserType(UserType.business);
+                  await replacePage(context, OnboardingPageBusiness());
+                },
+                child: Text('Business'),
+              ),
+              SizedBox(
+                height: 50.0,
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
