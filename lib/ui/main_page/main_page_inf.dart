2c9ca4676e11414d68de6f60483e73cb4949e222
Thomas Burkhart <github@burkharts.net>
2018-11-02 19:50:39 +0100

    working on InfMainScreen

--- lib/ui/main_page/main_page_inf.dart
+++ lib/ui/main_page/main_page_inf.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:inf/ui/widgets/auth_state_listener_mixin.dart';
+import 'package:inf/ui/widgets/connection_builder.dart';
 import 'package:inf/ui/widgets/routes.dart';
 
 class MainPageInf extends StatefulWidget {
@@ -14,11 +16,79 @@ class MainPageInf extends StatefulWidget {
 }
 
 class _MainPageInfState extends State<MainPageInf>
-    with AuthStateListenerMixin<MainPageInf> {
+    with AuthStateListenerMixin<MainPageInf>, SingleTickerProviderStateMixin {
+  TabController tabController;
+
+  _MainPageInfState() {
+    tabController = new TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Influencer MainPage'),
+    return ConnectionBuilder(builder: (contex, connectionState) {
+      return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SvgPicture.asset(
+                'assets/images/menu_icon.svg',
+              ),
+            ),
+            centerTitle: true,
+            title: SvgPicture.asset(
+              'assets/images/INF.svg',
+              height: 33.8,
+              fit: BoxFit.fill,
+              allowDrawingOutsideViewBox: true,
+            ),
+          ),
+          body: buildBody(),
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  title: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Offers'),
+                  ),
+                  icon: SvgPicture.asset(
+                    'assets/images/chat_icon.svg',
+                    height: 30.0,
+                  )),
+              BottomNavigationBarItem(
+                  title: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Proposals'),
+                  ),
+                  icon: SvgPicture.asset('assets/images/proposal_icon.svg',
+                      height: 30.0)),
+              BottomNavigationBarItem(
+                  title: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Agreements'),
+                  ),
+                  icon: SvgPicture.asset('assets/images/agreements.svg',
+                      height: 30.0)),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget buildBody() {
+    return Column(
+      children: <Widget>[
+        Text('Influencer MainPage'),
+        TabBar(controller: tabController,
+          tabs: <Widget>[
+            Text(
+              'MAP',
+            ),
+            Text('BROWSE')
+          ],
+        )
+      ],
     );
   }
 }
