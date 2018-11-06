4d8b9abb7be955ce118146f57b82af5b7f4b8ac1
Simon Lightfoot <simon@devangels.london>
2018-11-06 01:58:01 +0000

    Welcome screen.

--- lib/backend/services/resource_service_.dart
+++ lib/backend/services/resource_service_.dart
@@ -1,4 +1,9 @@
-abstract class ResourceService
-{
-  Future<List<String>> getWelcomePageProfileImages();
+abstract class ResourceService {
+  Stream<WelcomePageImages> getWelcomePageProfileImages();
+}
+
+class WelcomePageImages {
+  final List<String> images;
+
+  WelcomePageImages(this.images);
 }
