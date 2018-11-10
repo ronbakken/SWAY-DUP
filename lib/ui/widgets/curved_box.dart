04ad0c06fe6c7743396e2b39008c7dbd12b51ff2
Thomas Burkhart <github@burkharts.net>
2018-11-10 14:53:32 +0100

    Extended curved box to be able to have curves at the top and the bottom

--- lib/ui/widgets/curved_box.dart
+++ lib/ui/widgets/curved_box.dart
@@ -7,6 +7,8 @@ class CurvedBox extends StatelessWidget {
     Key key,
     this.curveFactor = 1.0,
     this.color,
+    this.top = false,
+    this.bottom = true,
     @required this.child,
   })  : assert(curveFactor != null),
         super(key: key);
@@ -14,6 +16,8 @@ class CurvedBox extends StatelessWidget {
   final double curveFactor;
   final Color color;
   final Widget child;
+  final bool top;
+  final bool bottom;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,8 @@ class CurvedBox extends StatelessWidget {
       painter: _CurvedBoxPainter(
         color: color ?? Theme.of(context).primaryColor,
         curveFactor: curveFactor,
+        bottom: bottom,
+        top: top
       ),
       child: child,
     );
@@ -28,21 +34,31 @@ class CurvedBox extends StatelessWidget {
 }
 
 class _CurvedBoxPainter extends CustomPainter {
-  _CurvedBoxPainter({
-    @required this.color,
-    this.curveFactor = 1.0,
-  });
+  _CurvedBoxPainter({@required this.color, this.curveFactor = 1.0, this.top, this.bottom});
 
   final Color color;
   final double curveFactor;
+  final bool top;
+  final bool bottom;
 
   @override
   void paint(Canvas canvas, Size size) {
     final curve = size.height * curveFactor;
     canvas.save();
     canvas.clipRect(Offset.zero & size);
-    canvas.drawArc(
-        Rect.fromLTRB(-curve, -size.height, size.width + curve, size.height), 0.0, pi, true, Paint()..color = color);
+    if (top && ! bottom) {
+      canvas.drawArc(
+          Rect.fromLTRB(-curve , 0, size.width + curve, 2* size.height), pi, 2*pi, true, Paint()..color = color);
+    }
+    else if (bottom && ! top) {
+      canvas.drawArc(
+          Rect.fromLTRB(-curve, -size.height, size.width + curve, size.height), 0.0, pi, true, Paint()..color = color);
+    }
+    else 
+    {
+      canvas.drawArc(
+          Rect.fromLTRB(-curve* 0.5, 0, size.width + curve *0.5, size.height), 0.0, 2* pi, true, Paint()..color = color);
+    }
     canvas.restore();
   }
 
