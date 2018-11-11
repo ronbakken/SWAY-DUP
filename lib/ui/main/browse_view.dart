2fc05d3a632c6cbfb8ded853a642af9b6b0af668
Thomas Burkhart <github@burkharts.net>
2018-11-11 17:54:35 +0100

    offer browse view done for now until dean creates a zeplin image of it

--- lib/ui/main/browse_view.dart
+++ lib/ui/main/browse_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:inf/app/assets.dart';
+import 'package:inf/app/theme.dart';
 import 'package:inf/backend/backend.dart';
 import 'package:inf/domain/domain.dart';
 import 'package:inf/ui/offers/offer_details_page.dart';
+import 'package:inf/ui/widgets/inf_asset_image.dart';
 import 'package:inf/ui/widgets/inf_image.dart';
 
 class BrowseView extends StatefulWidget {
@@ -52,33 +55,57 @@ class _OfferListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
-      child: InkResponse(
-        onTap: () => Navigator.of(context).push(OfferDetailsPage.route(offer)),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: InfImage(
-                lowRes: offer.thumbnailLowRes,
-                imageUrl: offer.thumbnailUrl,
+    return Material(
+      color: AppTheme.grey,
+      elevation: 2.0,
+      borderRadius: BorderRadius.circular(5.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+        child: InkResponse(
+          onTap: () => Navigator.of(context).push(OfferDetailsPage.route(offer)),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: AspectRatio(
+                  aspectRatio: 1.0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5.0),
+                      border: Border.all(color: Colors.black),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: InfImage(
+                        lowRes: offer.thumbnailLowRes,
+                        imageUrl: offer.thumbnailUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(width: 10.0),
-            Expanded(
-              flex: 5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(offer.title),
-                  SizedBox(height: 10.0),
-                  Text(offer.description),
-                ],
+              SizedBox(width: 16.0),
+              Expanded(
+                flex: 10,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Text(offer.title, textScaleFactor: 1.2, style: TextStyle(color: Colors.white)),
+                        SizedBox(width: 10.0),
+                        InfAssetImage(AppLogo.getDeliverableChannel(offer.deliverables[0].channel,), width: 20.0,)
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Text(offer.description, style: TextStyle(color: AppTheme.white30)),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
