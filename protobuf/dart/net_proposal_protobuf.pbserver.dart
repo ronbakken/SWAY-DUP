a5a2114ab92f276c187fc92cba6538648ec485bf
NO-BREAK SPACE OÜ <support@no-break.space>
2018-11-16 22:56:14 +0000

    Compile protobuf

---
+++ protobuf/dart/net_proposal_protobuf.pbserver.dart
@@ -0,0 +1,7 @@
+///
+//  Generated code. Do not modify.
+//  source: net_proposal_protobuf.proto
+///
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
+
+export 'net_proposal_protobuf.pb.dart';
