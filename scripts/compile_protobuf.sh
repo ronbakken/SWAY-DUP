f27a9be1ca193f0642d772edc1a42f5eb50bb06e
NO-BREAK SPACE OÜ <support@no-break.space>
2018-11-16 22:33:00 +0000

    Update protobuf

--- scripts/compile_protobuf.sh
+++ scripts/compile_protobuf.sh
@@ -5,23 +5,24 @@ set -x
 # pub global activate protoc_plugin
 
 
-cd ~/infcommon/
+cd ~/inf_common/
 git pull --recurse-submodules
 git add *
 git commit -m "Update protobuf"
 
 cd protobuf
 protoc --dart_out=./dart/ *.proto
-protoc --csharp_out=./cs/ *.proto
+# protoc --csharp_out=./cs/ *.proto
 cd dart
 find . -name '*.dart' | xargs -n 1 dartfmt -w
+sed -f ../enum_sed.txt -i enum_protobuf.pbenum.dart
 cd ..
 git add *
-cd ../config
-cp ../protobuf/dart/config_*.dart lib/protobuf/
-cp ../protobuf/dart/data_*.dart lib/protobuf/
-cp ../protobuf/dart/enum_*.dart lib/protobuf/
-git add *
+# cd ../config
+# cp ../protobuf/dart/config_*.dart lib/protobuf/
+# cp ../protobuf/dart/data_*.dart lib/protobuf/
+# cp ../protobuf/dart/enum_*.dart lib/protobuf/
+# git add *
 # cd ../api
 # cp ../protobuf/*.dart lib/
 # git add *
@@ -32,30 +33,30 @@ git push
 git status
 
 
-cd ~/infclient
-git pull --recurse-submodules
-git add *
-git commit -m "Update protobuf"
+# cd ~/infclient
+# git pull --recurse-submodules
+# git add *
+# git commit -m "Update protobuf"
+# 
+# cp ~/infcommon/protobuf/dart/*.dart infapp/lib/protobuf/
+# git add *
+# git commit -m "Compile protobuf"
+# 
+# git push
+# git status
+# 
+# 
+# cd ~/infserver
+# git pull --recurse-submodules
+# git add *
+# git commit -m "Update protobuf"
+# 
+# cp ~/infcommon/protobuf/dart/*.dart api/lib/protobuf/
+# git add *
+# git commit -m "Compile protobuf"
+# 
+# git push
+# git status
 
-cp ~/infcommon/protobuf/dart/*.dart infapp/lib/protobuf/
-git add *
-git commit -m "Compile protobuf"
 
-git push
-git status
-
-
-cd ~/infserver
-git pull --recurse-submodules
-git add *
-git commit -m "Update protobuf"
-
-cp ~/infcommon/protobuf/dart/*.dart api/lib/protobuf/
-git add *
-git commit -m "Compile protobuf"
-
-git push
-git status
-
-
-cd ~/infcommon/scripts
+cd ~/inf_common/scripts
--- scripts/light_compile_protobuf.sh
+++ scripts/light_compile_protobuf.sh
@@ -3,10 +3,11 @@
 cd ../protobuf
 
 protoc --dart_out=./dart/ *.proto
-protoc --csharp_out=./cs/ *.proto
+# protoc --csharp_out=./cs/ *.proto
 
 cd dart
 find . -name '*.dart' | xargs -n 1 dartfmt -w
+sed -f ../enum_sed.txt -i enum_protobuf.pbenum.dart
 cd ..
 
 cd ../scripts
