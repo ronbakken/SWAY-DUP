a70cbbdb81cc6eab7a7a96447ba97af4101d2a7e
Thomas Burkhart <github@burkharts.net>
2018-11-21 18:53:00 +0100

    added mapbounderies and markerIds to MapMarker

--- lib/domain/map_marker.dart
+++ lib/domain/map_marker.dart
@@ -3,13 +3,22 @@ import 'package:inf/domain/user.dart';
 enum MapMarkerType { cluster, user, offer }
 
 class MapMarker {
+  int id;
   final MapMarkerType type;
   final double latitude;
   final double longitude;
 
   // only valid for type cluster
   final int clusterCount;
+  final double clusterBounderyNorthWestLatitude;
+  final double clusterBounderyNorthWestlongitude;
+  final double clusterBounderySouthEasrLatitude;
+  final double clusterBounderySouthEasrlongitude;
 
+  // In case of clustering we need the ids of the clustered markers so that we can animate 
+  // From the clusterMarker to the individual markers
+  List<int> clusteredMapMarkerIds;
+ 
   // only valid for type offer
   final int offerId;
   final bool isDirectOffer;
@@ -23,6 +32,11 @@ class MapMarker {
     this.latitude,
     this.longitude,
     this.clusterCount,
+    this.clusteredMapMarkerIds,
+    this.clusterBounderyNorthWestLatitude,
+    this.clusterBounderyNorthWestlongitude,
+    this.clusterBounderySouthEasrLatitude,
+    this.clusterBounderySouthEasrlongitude,
     this.offerId,
     this.isDirectOffer,
     this.userId,
