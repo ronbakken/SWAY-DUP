5951d26beb09b5cc91d0e3787b88bc00249fd691
Jan Boon <kaetemi@no-break.space>
2018-11-25 14:12:51 +0000

    Update protobuf

--- lib/inf_common.dart
+++ lib/inf_common.dart
@@ -16,6 +16,7 @@ export 'package:inf_common/src/net_storage_protobuf.pb.dart';
 export 'package:inf_common/src/net_get_protobuf.pb.dart';
 export 'package:inf_common/src/net_push_protobuf.pb.dart';
 
+export 'package:inf_common/src/net_account_protobuf.pb.dart';
 export 'package:inf_common/src/net_profile_protobuf.pb.dart';
 export 'package:inf_common/src/net_offer_protobuf.pb.dart';
 export 'package:inf_common/src/net_proposal_protobuf.pb.dart';
