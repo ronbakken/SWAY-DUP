8bbf2c68254479ead2ec93a03c780d991d9120aa
Thomas Burkhart <github@burkharts.net>
2018-12-11 11:05:25 +0100

    small fix

--- lib/ui/widgets/category_button.dart
+++ lib/ui/widgets/category_button.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:inf/app/theme.dart';
 
-
 /// This widget is used to display Toplevel categories or Content Types.
 class CategoryButton extends StatelessWidget {
   /// if [selectedSubCategories] == null or == 0 no circle with number will be displayed
   final int selectedSubCategories;
+
   /// if this is true the button will be drawn blue
   final bool selected;
   final Widget child;
@@ -31,30 +31,34 @@ class CategoryButton extends StatelessWidget {
         Expanded(
           child: InkResponse(
             onTap: onTap,
-            child: AspectRatio(aspectRatio: 1.0,
-                          child: Center(
-                            child: Stack(fit: StackFit.passthrough,
-                children: <Widget>[
-                  Container(
-                    width: 2 * radius,
-                    height: 2 * radius,
-                    padding: const EdgeInsets.all(4.0),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: selectedSubCategories > 0 || selected
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Center(
+                child: Stack(
+                  fit: StackFit.passthrough,
+                  overflow: Overflow.visible,
+                  children: <Widget>[
+                    Container(
+                      width: 2 * radius,
+                      height: 2 * radius,
+                      padding: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: selectedSubCategories > 0 || selected
                             ? AppTheme.lightBlue
                             : AppTheme.grey,
-                      shape: BoxShape.circle,
-                      border: Border.all(
+                        shape: BoxShape.circle,
+                        border: Border.all(
                             width: 2.0,
                             color: selectedSubCategories > 0
                                 ? Colors.white
                                 : AppTheme.grey),
+                      ),
+                      child: child,
                     ),
-                    child: child,
-                  ),
-                  (selectedSubCategories != null) && (selectedSubCategories > 0)
-                      ? Positioned(
+                    (selectedSubCategories != null) &&
+                            (selectedSubCategories > 0)
+                        ? Positioned(
                             bottom: -4,
                             right: -12,
                             child: Container(
@@ -64,15 +68,15 @@ class CategoryButton extends StatelessWidget {
                               decoration: BoxDecoration(
                                   shape: BoxShape.circle,
                                   color: AppTheme.red,
-                                  border:
-                                      Border.all(color: Colors.black, width: 2.0)),
+                                  border: Border.all(
+                                      color: Colors.black, width: 2.0)),
                               child: Text('$selectedSubCategories'),
                             ),
-                        )
-                      : SizedBox(),
-                ],
+                          )
+                        : SizedBox(),
+                  ],
+                ),
               ),
-                          ),
             ),
           ),
         ),
