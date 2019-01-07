8bada0d11a9754bb78e8ff442f2da9750d489310
Jan Boon <kaetemi@no-break.space>
2019-01-07 16:29:26 +0800

    Update protobuf

--- lib/src/service_api_session.pbenum.dart
+++
@@ -1,5 +0,0 @@
-///
-//  Generated code. Do not modify.
-//  source: service_api_session.proto
-///
-// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
--- lib/src/service_api_session.pbserver.dart
+++
@@ -1,7 +0,0 @@
-///
-//  Generated code. Do not modify.
-//  source: service_api_session.proto
-///
-// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
-
-export 'service_api_session.pb.dart';
