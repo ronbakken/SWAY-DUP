e92755d24330eab84f8dd4f2ce0747a58a640b72
Thomas Burkhart <github@burkharts.net>
2019-01-09 11:58:39 +0100

    added missing import

--- android/app/src/main/java/app/inf/mobile/MainActivity.java
+++ android/app/src/main/java/app/inf/mobile/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.Window;
 
 import io.flutter.app.FlutterActivity;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 
