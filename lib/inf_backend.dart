3bc714586a669e73ce3501adec6abfe6159cf779
Jan Boon <jan.boon@kaetemi.be>
2019-01-09 15:02:07 +0800

    Push service

---
+++ lib/inf_backend.dart
@@ -0,0 +1,14 @@
+/*
+INF Marketplace
+Copyright (C) 2018  INF Marketplace LLC
+Author: Jan Boon <jan.boon@kaetemi.be>
+*/
+
+library inf_common;
+
+export 'package:inf_common/inf_common.dart';
+
+export 'package:inf_common/src/backend_push.pb.dart';
+export 'package:inf_common/src/backend_push.pbgrpc.dart';
+
+/* end of file */
