bd3f80ccc8c4130a0d27f45b9d5c4eed1e4a02fd
Jan Boon <kaetemi@no-break.space>
2019-01-10 10:57:19 +0800

    Compile protobuf

---
+++ lib/src/backend_jwt.pbjson.dart
@@ -0,0 +1,30 @@
+///
+//  Generated code. Do not modify.
+//  source: backend_jwt.proto
+///
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
+
+const ReqSign$json = const {
+  '1': 'ReqSign',
+  '2': const [
+    const {'1': 'claim', '3': 1, '4': 1, '5': 9, '10': 'claim'},
+  ],
+};
+
+const ResSign$json = const {
+  '1': 'ResSign',
+  '2': const [
+    const {'1': 'token', '3': 1, '4': 1, '5': 9, '10': 'token'},
+  ],
+};
+
+const ReqGetKeyStore$json = const {
+  '1': 'ReqGetKeyStore',
+};
+
+const ResGetKeyStore$json = const {
+  '1': 'ResGetKeyStore',
+  '2': const [
+    const {'1': 'jwks', '3': 1, '4': 1, '5': 9, '10': 'jwks'},
+  ],
+};
