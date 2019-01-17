01c060a78daf8bfb6ad04e916e57caeac7fd1317
Jan Boon <kaetemi@no-break.space>
2019-01-17 15:43:08 +0800

    Test apply for offer without negotiation

--- sample_api_direct/test/proposal_business_test.dart
+++ sample_api_direct/test/proposal_business_test.dart
@@ -63,6 +63,7 @@ Future<void> main() async {
   ApiAccountClient businessAccountClient;
   DataAccount influencerAccount;
   DataAccount businessAccount;
+  DataOffer offer;
 
   final NetSessionCreate sessionCreateRequest = NetSessionCreate();
   sessionCreateRequest.domain = config.services.domain;
@@ -71,6 +72,9 @@ Future<void> main() async {
   sessionCreateRequest.deviceName = 'oauth_test';
   sessionCreateRequest.deviceInfo = '{}';
   sessionCreateRequest.freeze();
+
+  int nextSessionGhostId =
+      (DateTime.now().toUtc().millisecondsSinceEpoch ~/ 1000) & 0xFFFFFFF;
 
   const bool devOverride = true;
   const String devIp = '192.168.43.202';
@@ -167,19 +171,12 @@ Future<void> main() async {
     expect(businessAccount.accountType, equals(AccountType.business));
     expect(influencerAccount.name, equals('INF Sandbox'));
     expect(businessAccount.name, equals('INF Boxsand'));
-  });
 
-  tearDownAll(() async {
-    await channel.terminate();
-  });
-
-  String imageUrl;
-  Uint8List imageData;
-  final Map<String, String> pexelsHeaders = <String, String>{
-    'authorization': serverConfig.services.pexelsKey,
-  };
-
-  test('Download testing image', () async {
+    final Map<String, String> pexelsHeaders = <String, String>{
+      'authorization': serverConfig.services.pexelsKey,
+    };
+    String imageUrl;
+    Uint8List imageData;
     final String url = serverConfig.services.pexelsApi +
         '/v1/curated?per_page=1&page=' +
         (random.nextInt(999) + 1).toString();
@@ -197,11 +194,7 @@ Future<void> main() async {
     expect(imageResponse.statusCode, equals(200));
     imageData = imageResponse.bodyBytes;
     expect(imageData, isNotEmpty);
-  });
 
-  NetUploadSigned uploadSigned;
-
-  test('Upload image', () async {
     final ApiStorageClient storageClient = ApiStorageClient(
       channel,
       options: grpc.CallOptions(metadata: <String, String>{
@@ -216,16 +209,17 @@ Future<void> main() async {
     log.fine(imageContentType);
     expect(imageContentType, startsWith('image/'));
 
+    // Upload image
     // Create a request to upload the file
-    final NetUploadImage request = NetUploadImage();
-    request.fileName = Uri.parse(imageUrl).path;
-    request.contentLength = imageData.length;
-    request.contentType = imageContentType;
-    request.contentSha256 = imageSha256.bytes;
-    request.freeze();
+    final NetUploadImage uploadRequest = NetUploadImage();
+    uploadRequest.fileName = Uri.parse(imageUrl).path;
+    uploadRequest.contentLength = imageData.length;
+    uploadRequest.contentType = imageContentType;
+    uploadRequest.contentSha256 = imageSha256.bytes;
+    uploadRequest.freeze();
 
     // log.finest(request);
-    uploadSigned = await storageClient.signImageUpload(request);
+    final uploadSigned = await storageClient.signImageUpload(uploadRequest);
     expect(uploadSigned, isNotNull);
     // log.fine(uploadSigned);
 
@@ -235,8 +229,9 @@ Future<void> main() async {
       // Upload the file
       final http.StreamedRequest httpRequest = http.StreamedRequest(
           uploadSigned.requestMethod, Uri.parse(uploadSigned.requestUrl));
-      httpRequest.headers['Content-Type'] = request.contentType;
-      httpRequest.headers['Content-Length'] = request.contentLength.toString();
+      httpRequest.headers['Content-Type'] = uploadRequest.contentType;
+      httpRequest.headers['Content-Length'] =
+          uploadRequest.contentLength.toString();
       // TODO: DigitalOcean Spaces doesn't process this option when in pre-signed URL query
       httpRequest.headers['x-amz-acl'] = 'public-read';
       final Future<http.StreamedResponse> futureResponse = httpRequest.send();
@@ -249,11 +244,8 @@ Future<void> main() async {
     expect(uploadSigned.uploadKey, isNotEmpty);
     expect(uploadSigned.coverUrl, isNotEmpty);
     expect(uploadSigned.thumbnailUrl, isNotEmpty);
-  });
 
-  DataOffer offer;
-
-  test('Create an offer', () async {
+    // Create offer
     final ApiOffersClient offersClient = ApiOffersClient(
       channel,
       options: grpc.CallOptions(metadata: <String, String>{
@@ -303,38 +295,128 @@ Future<void> main() async {
     createOffer.offer.terms.rewardItemOrServiceValue =
         (random.nextInt(20) + 1) * 1000;
 
-    final NetOffer response = await offersClient.create(createOffer);
-    expect(response.hasOffer(), isTrue);
+    final NetOffer offerResponse = await offersClient.create(createOffer);
+    expect(offerResponse.hasOffer(), isTrue);
 
-    offer = response.offer;
+    offer = offerResponse.offer;
     expect(offer.offerId, isNot(equals(Int64.ZERO)));
     expect(offer.title, equals(createOffer.offer.title));
     expect(offer.description, equals(createOffer.offer.description));
     expect(offer.hasTerms(), isTrue);
   });
 
-  DataOffer offerAsInfluencer;
+  tearDownAll(() async {
+    await channel.terminate();
+  });
 
-  test('Influencer fetches the offer', () async {
-    final ApiOffersClient offersClient = ApiOffersClient(
-      channel,
-      options: grpc.CallOptions(metadata: <String, String>{
-        'authorization': 'Bearer $influencerAccessToken'
-      }),
-    );
-    final NetGetOffer request = NetGetOffer();
-    request.offerId = offer.offerId;
-    final NetOffer response = await offersClient.get(request);
-    expect(response.hasOffer(), isTrue);
-    expect(response.detail, isTrue);
-    expect(response.summary, isTrue);
-    expect(response.state, isTrue);
-    expect(response.offer.offerId, equals(offer.offerId));
-    expect(response.offer.offerId, isNot(equals(Int64.ZERO)));
-    expect(response.offer.title, equals(offer.title));
-    expect(response.offer.hasProposalId(),
-        isFalse); // TODO: Verify this goes true after proposing
-    offerAsInfluencer = offer;
+  group('Apply and deal without negotiation', () {
+    DataOffer offerAsInfluencer;
+
+    test('Influencer fetches the offer', () async {
+      offerAsInfluencer = null;
+      final ApiOffersClient offersClient = ApiOffersClient(
+        channel,
+        options: grpc.CallOptions(metadata: <String, String>{
+          'authorization': 'Bearer $influencerAccessToken'
+        }),
+      );
+      final NetGetOffer request = NetGetOffer();
+      request.offerId = offer.offerId;
+      final NetOffer response = await offersClient.get(request);
+      expect(response.hasOffer(), isTrue);
+      expect(response.detail, isTrue);
+      expect(response.summary, isTrue);
+      expect(response.state, isTrue);
+      expect(response.offer.offerId, equals(offer.offerId));
+      expect(response.offer.offerId, isNot(equals(Int64.ZERO)));
+      expect(response.offer.title, equals(offer.title));
+      expect(response.offer.hasProposalId(),
+          isFalse); // TODO: Verify this goes true after proposing
+      offerAsInfluencer = offer;
+    });
+
+    // TODO: Validate push messages are sent and received (including on secondary sessions)
+
+    test('Business cannot apply to a business offer', () async {
+      final NetApplyProposal request = NetApplyProposal();
+      request.offerId = offerAsInfluencer.offerId;
+      request.remarks = 'I am the most excellent ${faker.job.title()}';
+      request.sessionGhostId = ++nextSessionGhostId;
+      request.freeze();
+      final ApiProposalsClient proposalsClient = ApiProposalsClient(
+        channel,
+        options: grpc.CallOptions(metadata: <String, String>{
+          'authorization': 'Bearer $businessAccessToken'
+        }),
+      );
+      expect(proposalsClient.apply(request),
+          throwsA(const TypeMatcher<grpc.GrpcError>()));
+    });
+
+    Int64 proposalId;
+    DataProposalChat termsChat;
+    DataProposalChat lastChat;
+    // DataProposalChat markerChat;
+
+    test('Apply for the offer without negotiation', () async {
+      proposalId = null;
+      termsChat = null;
+      lastChat = null;
+      // markerChat = null;
+      final NetApplyProposal request = NetApplyProposal();
+      request.offerId = offerAsInfluencer.offerId;
+      request.remarks = 'I am the most excellent ${faker.job.title()}';
+      request.sessionGhostId = ++nextSessionGhostId;
+      request.freeze();
+      final ApiProposalsClient proposalsClient = ApiProposalsClient(
+        channel,
+        options: grpc.CallOptions(metadata: <String, String>{
+          'authorization': 'Bearer $influencerAccessToken'
+        }),
+      );
+      final NetProposal proposalResponse = await proposalsClient.apply(request);
+      proposalId = proposalResponse.proposal.proposalId;
+      expect(proposalResponse.hasProposal(), isTrue);
+      expect(proposalResponse.proposal.proposalId, isNot(equals(Int64.ZERO)));
+      expect(
+          proposalResponse.proposal.businessName, equals(businessAccount.name));
+      expect(proposalResponse.proposal.influencerName,
+          equals(influencerAccount.name));
+      expect(proposalResponse.proposal.offerTitle,
+          equals(offerAsInfluencer.title));
+      expect(proposalResponse.proposal.state, equals(ProposalState.proposing));
+      expect(proposalResponse.proposal.termsChatId, isNot(equals(Int64.ZERO)));
+      expect(proposalResponse.proposal.lastChatId, isNot(equals(Int64.ZERO)));
+      for (DataProposalChat chat in proposalResponse.chats) {
+        if (chat.chatId == proposalResponse.proposal.termsChatId) {
+          // The terms chat exists
+          termsChat = chat;
+        }
+        if (chat.chatId == proposalResponse.proposal.lastChatId) {
+          // Last chat is valid
+          lastChat = chat;
+        }
+        /*
+        // Disabled. For now the terms chat counts as marker.
+        if (chat.type == ProposalChatType.marker &&
+            chat.marker == ProposalChatMarker.applied) {
+          // There must be a marker chat to signal the new proposal
+          markerChat = chat;
+        }
+        */
+      }
+      expect(termsChat, isNotNull);
+      expect(lastChat, isNotNull);
+      // expect(markerChat, isNotNull);
+      // Terms without negotiation must match the offer terms
+      expect(termsChat.terms, equals(offerAsInfluencer.terms));
+      // Last chat must be the highest chat id
+      for (DataProposalChat chat in proposalResponse.chats) {
+        expect(chat.chatId, lessThanOrEqualTo(lastChat.chatId));
+      }
+      // Terms is the last chat id
+      expect(termsChat.chatId, equals(lastChat.chatId));
+    });
   });
 }
 
