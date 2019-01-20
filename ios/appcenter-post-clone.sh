c74808b61a3b89d5ec1a5d62b88ce29746471498
Jan Boon <jan.boon@kaetemi.be>
2019-01-20 13:43:06 +0800

    Fix

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -4,14 +4,14 @@ set -x
 
 echo `pwd`
 mkdir ~/.ssh
-cp ./ssh/* ~/.ssh/
+cp ../ssh/* ~/.ssh/
 chmod 600 ~/.ssh/*
 ls -al ~/.ssh/
 
 # Use sandbox configuration
-rm assets/config.bin
-cp assets/config_ats3.bin assets/config.bin
-# cp android/key.properties.infsandbox android/key.properties
+rm ../assets/config.bin
+cp ../assets/config_ats3.bin ../assets/config.bin
+# cp ../android/key.properties.infsandbox ../android/key.properties
 
 cd ..
 git clone -b stable https://github.com/flutter/flutter.git
