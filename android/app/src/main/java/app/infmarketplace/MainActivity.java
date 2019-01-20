a1474a65230249e88ae20fb5afb90fc0c852887c
Jan Boon <jan.boon@kaetemi.be>
2019-01-20 15:02:00 +0800

    Add app center integration

--- android/app/src/main/java/app/infmarketplace/MainActivity.java
+++ android/app/src/main/java/app/infmarketplace/MainActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    AppCenter.start(getApplication(), "17c4d591-44f7-40f0-aabd-d36a1e4929cf", Analytics.class, Crashes.class);
   }
 }
