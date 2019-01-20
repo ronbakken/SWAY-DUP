e2eab203c88d3bb9b5e7b9e0b1849c7f627ac1db
Jan Boon <jan.boon@kaetemi.be>
2019-01-20 13:28:07 +0800

    App Center script

---
+++ ios/appcenter-post-clone.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+ls -al ../.ssh/
+cat ../.ssh/config
+cp ./ssh/* ../.ssh/
+ls -al ../.ssh/
+cat ../.ssh/config
+chmod 600 ../.ssh/*
+ls -al ../.ssh/
+rm assets/config.bin
+
+# Use sandbox configuration
+cp assets/config_ats3.bin assets/config.bin
+# cp android/key.properties.infsandbox android/key.properties
+
+cd ..
+git clone -b stable https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+echo $PATH
+
+flutter doctor
+
+# brew update
+# brew install --HEAD usbmuxd
+# brew link usbmuxd
+# brew install --HEAD libimobiledevice
+# brew install ideviceinstaller ios-deploy cocoapods
+# pod setup
+
+pod repo update
+
+flutter build ios --release --no-codesign
