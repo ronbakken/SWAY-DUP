623b6ed2c21e36a7c943b2c05468fdc45deb23da
Jan Boon <kaetemi@no-break.space>
2019-01-26 16:58:28 +0800

    Lock firebase messaging version down for now. Latest requires AndroidX, but local notification plugin doesn't support that.

--- android/app/appcenter-pre-build.sh
+++ android/app/appcenter-pre-build.sh
@@ -7,6 +7,8 @@ export PATH=`pwd`/flutter/bin:$PATH
 export FLUTTER_ROOT=`pwd`/flutter
 
 flutter doctor
+flutter packages get
+cat pubspec.lock
 
 flutter build apk --release --build-name=1.0.$APPCENTER_BUILD_ID --build-number=$APPCENTER_BUILD_ID
 
--- ios/appcenter-pre-build.sh
+++ ios/appcenter-pre-build.sh
@@ -7,6 +7,8 @@ export PATH=`pwd`/flutter/bin:$PATH
 export FLUTTER_ROOT=`pwd`/flutter
 
 flutter doctor
+flutter packages get
+cat pubspec.lock
 
 pod repo update
 flutter build ios --release --no-codesign --build-name=1.0.$APPCENTER_BUILD_ID --build-number=$APPCENTER_BUILD_ID
