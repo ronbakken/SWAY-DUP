afd1374a89f26ee453b215047cb7e6f3b35a0ea9
unknown <kent.boogaart@gmail.com>
2019-02-04 16:47:52 +1000

    Improve QuadKey implementation for our needs

--- src/inf_server/Utility/Mapping/QuadKey.cs
+++ src/inf_server/Utility/Mapping/QuadKey.cs
@@ -1,50 +1,234 @@
 ﻿using System;
-using System.Text;
-using Genesis.Ensure;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Utility.Mapping
 {
-    public sealed class QuadKey : IEquatable<QuadKey>
+    // See https://docs.microsoft.com/en-us/bingmaps/articles/bing-maps-tile-system.
+    public struct QuadKey : IEquatable<QuadKey>
     {
-        private readonly string value;
+        // A default, invalid, instance of QuadKey.
+        public static readonly QuadKey Default = new QuadKey();
 
-        public QuadKey(string value)
+        // 6 LSBs hold the level of detail (6 bits is more than enough, but 5 is too few)
+        // Bits 7 and 8 hold level 1 value, bits 9 and 10 level 2, etcetera.
+        private readonly ulong rawValue;
+
+        public QuadKey(ulong rawValue)
         {
-            if (string.IsNullOrEmpty(value))
-            {
-                throw new ArgumentException(nameof(value));
-            }
+            this.rawValue = rawValue;
+        }
 
-            for (var i = 0; i < value.Length; ++i)
-            {
-                var ch = value[i];
+        public ulong RawValue => this.rawValue;
 
-                if (ch < '0' || ch > '3')
+        public bool IsValid => this.LevelCount > 0 && this.LevelCount <= 29 &&  (this.LevelCount == 29 || (this.rawValue >> (6 + this.LevelCount * 2) == 0));
+
+        public int LevelCount => (int)(this.rawValue & 0b111111);
+
+        // Gets the parent QuadKey, which will be QuadKey.Default if LevelCount is 1.
+        public QuadKey Parent
+        {
+            get
+            {
+                EnsureValid();
+
+                if (this.LevelCount == 1)
                 {
-                    throw new ArgumentException(nameof(value));
+                    return QuadKey.Default;
                 }
+
+                var newRawValue = SetQuadValue(this.rawValue, this.LevelCount - 1, 0);
+                newRawValue &= ~0b111111UL;
+                newRawValue |= (uint)(this.LevelCount - 1);
+                return new QuadKey(newRawValue);
             }
-
-            this.value = value;
         }
 
-        public QuadKey(double latitude, double longitude, int level)
+        // Gets the QuadKey to the north of this QuadKey, which will be QuadKey.Default if there is no QuadKey further north.
+        public QuadKey North
         {
-            GetPixelCoordinatesFromLatLong(latitude, longitude, level, out var pixelX, out var pixelY);
-            GetTileCoordinatesFromPixelCoordinates(pixelX, pixelY, out var tileX, out var tileY);
-            this.value = GetQuadKeyFromTileCoordinates(tileX, tileY, level);
+            get
+            {
+                EnsureValid();
+
+                var newRawValue = this.rawValue;
+                var currentQuadKey = this;
+
+                while (true)
+                {
+                    var currentQuadValue = GetQuadValue(newRawValue, currentQuadKey.LevelCount - 1);
+                    var isOnNorthernEdge = currentQuadValue == 0 || currentQuadValue == 1;
+
+                    if (isOnNorthernEdge)
+                    {
+                        if (currentQuadKey.LevelCount == 1)
+                        {
+                            // Can't go any further north
+                            return QuadKey.Default;
+                        }
+
+                        // For quads on the northern edge, 0s change to 2, 1s change to 3
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 0 ? 2 : 3);
+                        currentQuadKey = currentQuadKey.Parent;
+                    }
+                    else
+                    {
+                        // For quads that are not on the northern edge, 2s change to 0, 3s change 1 (and we're done at this point)
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 2 ? 0 : 1);
+                        break;
+                    }
+                }
+
+                return new QuadKey(newRawValue);
+            }
         }
 
-        public string Value => this.value;
-
-        public bool Contains(QuadKey other)
+        // Gets the QuadKey to the south of this QuadKey, which will be QuadKey.Default if there is no QuadKey further south.
+        public QuadKey South
         {
-            Ensure.ArgumentNotNull(other, nameof(other));
-            return other.value.StartsWith(this.value);
+            get
+            {
+                EnsureValid();
+
+                var newRawValue = this.rawValue;
+                var currentQuadKey = this;
+
+                while (true)
+                {
+                    var currentQuadValue = GetQuadValue(newRawValue, currentQuadKey.LevelCount - 1);
+                    var isOnSouthernEdge = currentQuadValue == 2 || currentQuadValue == 3;
+
+                    if (isOnSouthernEdge)
+                    {
+                        if (currentQuadKey.LevelCount == 1)
+                        {
+                            // Can't go any further south
+                            return QuadKey.Default;
+                        }
+
+                        // For quads on the southern edge, 2s change to 0, 3s change to 1
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 2 ? 0 : 1);
+                        currentQuadKey = currentQuadKey.Parent;
+                    }
+                    else
+                    {
+                        // For quads that are not on the southern edge, 0s change to 2, 1s change 3 (and we're done at this point)
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 0 ? 2 : 3);
+                        break;
+                    }
+                }
+
+                return new QuadKey(newRawValue);
+            }
+        }
+
+        // Gets the QuadKey to the east of this QuadKey, which will be QuadKey.Default if there is no QuadKey further east.
+        public QuadKey East
+        {
+            get
+            {
+                EnsureValid();
+
+                var newRawValue = this.rawValue;
+                var currentQuadKey = this;
+
+                while (true)
+                {
+                    var currentQuadValue = GetQuadValue(newRawValue, currentQuadKey.LevelCount - 1);
+                    var isOnEasternEdge = currentQuadValue == 1 || currentQuadValue == 3;
+
+                    if (isOnEasternEdge)
+                    {
+                        if (currentQuadKey.LevelCount == 1)
+                        {
+                            // Can't go any further east
+                            return QuadKey.Default;
+                        }
+
+                        // For quads on the eastern edge, 1s change to 0, 3s change to 2
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 1 ? 0 : 2);
+                        currentQuadKey = currentQuadKey.Parent;
+                    }
+                    else
+                    {
+                        // For quads that are not on the eastern edge, 0s change to 1, 2s change 3 (and we're done at this point)
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 0 ? 1 : 3);
+                        break;
+                    }
+                }
+
+                return new QuadKey(newRawValue);
+            }
+        }
+
+        // Gets the QuadKey to the west of this QuadKey, which will be QuadKey.Default if there is no QuadKey further west.
+        public QuadKey West
+        {
+            get
+            {
+                EnsureValid();
+
+                var newRawValue = this.rawValue;
+                var currentQuadKey = this;
+
+                while (true)
+                {
+                    var currentQuadValue = GetQuadValue(newRawValue, currentQuadKey.LevelCount - 1);
+                    var isOnWesternEdge = currentQuadValue == 0 || currentQuadValue == 2;
+
+                    if (isOnWesternEdge)
+                    {
+                        if (currentQuadKey.LevelCount == 1)
+                        {
+                            // Can't go any further west
+                            return QuadKey.Default;
+                        }
+
+                        // For quads on the western edge, 0s change to 1, 2s change to 3
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 0 ? 1 : 3);
+                        currentQuadKey = currentQuadKey.Parent;
+                    }
+                    else
+                    {
+                        // For quads that are not on the western edge, 1s change to 0, 3s change 2 (and we're done at this point)
+                        newRawValue = SetQuadValue(newRawValue, currentQuadKey.LevelCount - 1, currentQuadValue == 1 ? 0 : 2);
+                        break;
+                    }
+                }
+
+                return new QuadKey(newRawValue);
+            }
+        }
+
+        // Gets the four child QuadKeys for this QuadKey.
+        public IEnumerable<QuadKey> GetChildren()
+        {
+            EnsureValid();
+
+            var newLevel = this.LevelCount + 1;
+            var newRawValue = this.rawValue;
+            newRawValue &= ~0b111111UL;
+            newRawValue |= (uint)newLevel;
+
+            // 0
+            newRawValue = SetQuadValue(newRawValue, this.LevelCount, 0);
+            yield return new QuadKey(newRawValue);
+
+            // 1
+            newRawValue = SetQuadValue(newRawValue, this.LevelCount, 1);
+            yield return new QuadKey(newRawValue);
+
+            // 2
+            newRawValue = SetQuadValue(newRawValue, this.LevelCount, 2);
+            yield return new QuadKey(newRawValue);
+
+            // 3
+            newRawValue = SetQuadValue(newRawValue, this.LevelCount, 3);
+            yield return new QuadKey(newRawValue);
         }
 
         public bool Equals(QuadKey other) =>
-            string.Equals(this.value, other?.value);
+            this.rawValue == other.rawValue;
 
         public override bool Equals(object obj)
         {
@@ -57,10 +241,168 @@ namespace Utility.Mapping
         }
 
         public override int GetHashCode() =>
-            this.value.GetHashCode();
+            this.rawValue.GetHashCode();
 
-        public override string ToString() =>
-            this.value;
+        public override string ToString()
+        {
+            if (!this.IsValid)
+            {
+                return "<<invalid>>";
+            }
+
+            var result = new char[this.LevelCount];
+
+            for (var i = 0; i < this.LevelCount; ++i)
+            {
+                var quadValue = GetQuadValue(rawValue, i);
+                var ch = (char)('0' + quadValue);
+                result[i] = ch;
+            }
+
+            return new string(result);
+        }
+
+        public static QuadKey From(double latitude, double longitude, int level)
+        {
+            GetPixelCoordinatesFromLatLong(latitude, longitude, level, out var pixelX, out var pixelY);
+            GetTileCoordinatesFromPixelCoordinates(pixelX, pixelY, out var tileX, out var tileY);
+            var rawValue = GetRawValueFromTileCoordinates(tileX, tileY, level);
+            return new QuadKey(rawValue);
+        }
+
+        public static QuadKey Parse(string input)
+        {
+            if (!TryParse(input, out var result))
+            {
+                throw new FormatException();
+            }
+
+            return result;
+        }
+
+        public static bool TryParse(string input, out QuadKey result)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                result = QuadKey.Default;
+                return false;
+            }
+
+            var rawValue = 0UL;
+
+            for (var i = 0; i < input.Length; ++i)
+            {
+                var ch = input[i];
+                var value = ch - '0';
+
+                if (value < 0 || value > 3)
+                {
+                    result = QuadKey.Default;
+                    return false;
+                }
+
+                rawValue = SetQuadValue(rawValue, i, value);
+            }
+
+            rawValue |= (uint)input.Length;
+            result = new QuadKey(rawValue);
+            return result.IsValid;
+        }
+
+        // Get all QuadKeys in a 2D range.
+        public static IEnumerable<QuadKey> GetRange(QuadKey northWest, QuadKey southEast)
+        {
+            northWest.EnsureValid();
+            southEast.EnsureValid();
+
+            var nwQuadKeyCoords = (x: northWest.GetDistanceFromWesternEdge(), y: northWest.GetDistanceFromNorthernEdge());
+            var seQuadKeyCoords = (x: southEast.GetDistanceFromWesternEdge(), y: southEast.GetDistanceFromNorthernEdge());
+
+            if (nwQuadKeyCoords.x > seQuadKeyCoords.x || nwQuadKeyCoords.y > seQuadKeyCoords.y)
+            {
+                throw new ArgumentException("northWest must be north-west of southEast.");
+            }
+
+            var currentQuadKey = northWest;
+
+            for (var i = nwQuadKeyCoords.y; i <= seQuadKeyCoords.y; ++i)
+            {
+                var snapshot = currentQuadKey;
+
+                for (var j = nwQuadKeyCoords.x; j <= seQuadKeyCoords.x; ++j)
+                {
+                    yield return currentQuadKey;
+                    currentQuadKey = currentQuadKey.East;
+                }
+
+                currentQuadKey = snapshot.South;
+            }
+        }
+
+        private void EnsureValid()
+        {
+            if (!this.IsValid)
+            {
+                throw new InvalidOperationException("QuadKey is not valid.");
+            }
+        }
+
+        // Get a level-sensitive distance from the western edge for this QuadKey. For example, a return value of 0 means it is on
+        // the western edge, a value of 2 means it is the third QuadKey from the western edge.
+        private int GetDistanceFromWesternEdge()
+        {
+            var distance = 0;
+
+            for (var i = 0; i < this.LevelCount; ++i)
+            {
+                var currentQuadValue = GetQuadValue(this.rawValue, i);
+
+                if (currentQuadValue == 1 || currentQuadValue == 3)
+                {
+                    distance += (int)Math.Pow(2, this.LevelCount - i - 1);
+                }
+            }
+
+            return distance;
+        }
+
+        // Get a level-sensitive distance from the northern edge for this QuadKey. For example, a return value of 0 means it is on
+        // the northern edge, a value of 2 means it is the third QuadKey from the northern edge.
+        private int GetDistanceFromNorthernEdge()
+        {
+            var distance = 0;
+
+            for (var i = 0; i < this.LevelCount; ++i)
+            {
+                var currentQuadValue = GetQuadValue(this.rawValue, i);
+
+                if (currentQuadValue == 2 || currentQuadValue == 3)
+                {
+                    distance += (int)Math.Pow(2, this.LevelCount - i - 1);
+                }
+            }
+
+            return distance;
+        }
+
+        // Get the value (0, 1, 2, or 3) for an individual quad. Quad numbers start at 0 (the most significant) and run up to Level - 1.
+        private static ulong GetQuadValue(ulong rawValue, int quadNumber)
+        {
+            Debug.Assert(quadNumber >= 0 && quadNumber <= 29);
+            var maskedValue = rawValue & (0b11UL << (6 + quadNumber * 2));
+            return maskedValue >> (6 + quadNumber * 2);
+        }
+
+        // Sets the value (0, 1, 2, 3) for an individual quad.
+        private static ulong SetQuadValue(ulong rawValue, int quadNumber, int value)
+        {
+            Debug.Assert(quadNumber >= 0 && quadNumber <= 29);
+            Debug.Assert(value >= 0 && value <= 3);
+            var newRawValue = rawValue;
+            newRawValue &= ~(0b11UL << (6 + quadNumber * 2));
+            newRawValue |= (ulong)value << (6 + quadNumber * 2);
+            return newRawValue;
+        }
 
         // Everything below is largely based on https://docs.microsoft.com/en-us/bingmaps/articles/bing-maps-tile-system.
         private const double earthRadius = 6378137;
@@ -95,30 +437,33 @@ namespace Utility.Mapping
             tileY = pixelY / 256;
         }
 
-        private static string GetQuadKeyFromTileCoordinates(int tileX, int tileY, int levelOfDetail)
+        private static ulong GetRawValueFromTileCoordinates(int tileX, int tileY, int levelOfDetail)
         {
-            var quadKey = new StringBuilder();
+            var rawValue = 0UL;
 
             for (int i = levelOfDetail; i > 0; i--)
             {
-                var digit = '0';
+                var value = 0UL;
                 var mask = 1 << (i - 1);
 
                 if ((tileX & mask) != 0)
                 {
-                    digit++;
+                    value++;
                 }
 
                 if ((tileY & mask) != 0)
                 {
-                    digit++;
-                    digit++;
+                    value++;
+                    value++;
                 }
 
-                quadKey.Append(digit);
+                var invertI = levelOfDetail - i;
+                rawValue |= (value << (6 + invertI * 2));
             }
 
-            return quadKey.ToString();
+            rawValue |= (uint)levelOfDetail;
+
+            return rawValue;
         }
     }
 }
--- src/inf_server/Utility/Mapping/QuadKeyUnitTests.cs
+++ src/inf_server/Utility/Mapping/QuadKeyUnitTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using Xunit;
 
 namespace Utility.Mapping
@@ -6,44 +6,155 @@ namespace Utility.Mapping
     public sealed class QuadKeyUnitTests
     {
         [Theory]
-        [InlineData(-34.633429521533216, 138.73932122222902, 17, "31122030121030001")]
-        [InlineData(51.49681999999999, 0.012860000000012306, 15, "120202020020003")]
-        [InlineData(51.49681999999999, 0.012860000000012306, 3, "120")]
-        public void quad_keys_calculate_correctly_from_latitude_longitude_and_level(double latitude, double longitude, int level, string expected)
+        [InlineData(0UL)]
+        [InlineData(1UL)]
+        [InlineData(348772231245300630UL)]
+        public void raw_value_returns_raw_value(ulong rawValue)
         {
-            var quadKey = new QuadKey(latitude, longitude, level);
-            Assert.Equal(expected, quadKey.ToString());
+            var sut = new QuadKey(rawValue);
+            Assert.Equal(rawValue, sut.RawValue);
         }
 
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("abc")]
-        [InlineData("4")]
-        [InlineData("01310172010")]
-        public void invalid_quad_key_values_are_rejected(string value)
+        [InlineData(0b0, false)]
+        [InlineData(0b1, true)]
+        [InlineData(0b011110, false)]
+        [InlineData(0b011101, true)]
+        [InlineData(0b1011000001, false)]
+        public void is_valid_returns_true_only_for_valid_quad_keys(ulong rawValue, bool expected)
         {
-            Assert.Throws<ArgumentException>(() => new QuadKey(value));
+            var sut = new QuadKey(rawValue);
+            Assert.Equal(expected, sut.IsValid);
         }
 
         [Theory]
-        [InlineData("0")]
-        [InlineData("1")]
-        [InlineData("2")]
-        [InlineData("3")]
-        [InlineData("3102221301001120131023102103100021121202031022113")]
-        public void valid_quad_key_values_are_accepted(string value)
+        [InlineData(0b000001, 1)]
+        [InlineData(0b000010, 2)]
+        [InlineData(0b1111000001, 1)]
+        [InlineData(0b000111, 7)]
+        public void level_returns_the_level(ulong rawValue, int expected)
         {
-            var sut = new QuadKey(value);
-            Assert.Equal(value, sut.Value);
+            var sut = new QuadKey(rawValue);
+            Assert.Equal(expected, sut.LevelCount);
         }
 
         [Theory]
-        [InlineData("0", "0", true)]
-        [InlineData("0", "1", false)]
-        [InlineData("0123", "0123", true)]
-        [InlineData("01230", "0123", false)]
-        public void equality_works_as_expected(string first, string second, bool expected)
+        [InlineData("01", "0")]
+        [InlineData("10", "1")]
+        [InlineData("2", "<<invalid>>")]
+        [InlineData("320121", "32012")]
+        public void parent_returns_the_parent_quad_key(string current, string expected)
+        {
+            var sut = QuadKey.Parse(current);
+            var parent = sut.Parent;
+
+            Assert.Equal(expected, parent.ToString());
+        }
+
+        [Theory]
+        [InlineData("0", new[] { "00", "01", "02", "03" })]
+        [InlineData("1", new[] { "10", "11", "12", "13" })]
+        [InlineData("3102", new[] { "31020", "31021", "31022", "31023" })]
+        public void get_children_returns_all_child_quad_keys(string current, string[] expected)
+        {
+            var sut = QuadKey.Parse(current);
+            var children = sut.GetChildren();
+
+            Assert.True(children.Select(child => child.ToString()).SequenceEqual(expected));
+        }
+
+        [Theory]
+        [InlineData("0", "<<invalid>>")]
+        [InlineData("2", "0")]
+        [InlineData("3", "1")]
+        [InlineData("32", "30")]
+        [InlineData("33", "31")]
+        [InlineData("30", "12")]
+        [InlineData("211", "033")]
+        [InlineData("313", "311")]
+        [InlineData("111", "<<invalid>>")]
+        public void north_returns_the_quad_key_to_the_north(string current, string expected)
+        {
+            var sut = QuadKey.Parse(current);
+            Assert.Equal(expected, sut.North.ToString());
+        }
+
+        [Theory]
+        [InlineData("2", "<<invalid>>")]
+        [InlineData("0", "2")]
+        [InlineData("1", "3")]
+        [InlineData("30", "32")]
+        [InlineData("31", "33")]
+        [InlineData("12", "30")]
+        [InlineData("033", "211")]
+        [InlineData("311", "313")]
+        [InlineData("333", "<<invalid>>")]
+        public void south_returns_the_quad_key_to_the_south(string current, string expected)
+        {
+            var sut = QuadKey.Parse(current);
+            Assert.Equal(expected, sut.South.ToString());
+        }
+
+        [Theory]
+        [InlineData("1", "<<invalid>>")]
+        [InlineData("0", "1")]
+        [InlineData("2", "3")]
+        [InlineData("30", "31")]
+        [InlineData("32", "33")]
+        [InlineData("03", "12")]
+        [InlineData("033", "122")]
+        [InlineData("332", "333")]
+        [InlineData("311", "<<invalid>>")]
+        public void east_returns_the_quad_key_to_the_east(string current, string expected)
+        {
+            var sut = QuadKey.Parse(current);
+            Assert.Equal(expected, sut.East.ToString());
+        }
+
+        [Theory]
+        [InlineData("0", "<<invalid>>")]
+        [InlineData("1", "0")]
+        [InlineData("3", "2")]
+        [InlineData("31", "30")]
+        [InlineData("33", "32")]
+        [InlineData("12", "03")]
+        [InlineData("122", "033")]
+        [InlineData("333", "332")]
+        [InlineData("222", "<<invalid>>")]
+        public void west_returns_the_quad_key_to_the_west(string current, string expected)
+        {
+            var sut = QuadKey.Parse(current);
+            Assert.Equal(expected, sut.West.ToString());
+        }
+
+        [Theory]
+        [InlineData("0", "0", new[] { "0" })]
+        [InlineData("1", "1", new[] { "1" })]
+        [InlineData("00", "00", new[] { "00" })]
+        [InlineData("0", "3", new[] { "0", "1", "2", "3" })]
+        [InlineData("030", "312", new[] { "030", "031", "120", "121", "130", "032", "033", "122", "123", "132", "210", "211", "300", "301", "310", "212", "213", "302", "303", "312" })]
+        public void get_range_gets_all_quad_keys_in_a_range(string northWest, string southEast, string[] expected)
+        {
+            var results = QuadKey.GetRange(QuadKey.Parse(northWest), QuadKey.Parse(southEast));
+            Assert.True(results.Select(result => result.ToString()).SequenceEqual(expected));
+        }
+
+        [Theory]
+        [InlineData(0b0, "<<invalid>>")]
+        [InlineData(0b1, "0")]
+        [InlineData(0b000100_10000100, "2010")]
+        public void to_string_returns_a_string_representation_of_the_quad_key(ulong rawValue, string expected)
+        {
+            var sut = new QuadKey(rawValue);
+            Assert.Equal(expected, sut.ToString());
+        }
+
+        [Theory]
+        [InlineData(0, 0, true)]
+        [InlineData(0, 1, false)]
+        [InlineData(0123, 0123, true)]
+        [InlineData(01230, 0123, false)]
+        public void equality_works_as_expected(ulong first, ulong second, bool expected)
         {
             var firstSut = new QuadKey(first);
             var secondSut = new QuadKey(second);
@@ -53,19 +164,26 @@ namespace Utility.Mapping
         }
 
         [Theory]
-        [InlineData("0", "0", true)]
-        [InlineData("0", "01", true)]
-        [InlineData("0", "1", false)]
-        [InlineData("0", "10", false)]
-        [InlineData("032", "0", false)]
-        [InlineData("032", "03", false)]
-        [InlineData("012320123013201231201203120121312312312332323222100", "0123201230132012312012031201213123123123323232221001", true)]
-        public void contains_works_as_expected(string first, string second, bool expected)
+        [InlineData(-34.633429521533216, 138.73932122222902, 17, "31122030121030001")]
+        [InlineData(51.49681999999999, 0.012860000000012306, 15, "120202020020003")]
+        [InlineData(51.49681999999999, 0.012860000000012306, 3, "120")]
+        [InlineData(51.49681999999999, 0.012860000000012306, 30, "<<invalid>>")]
+        public void from_works_as_expected(double latitude, double longitude, int level, string expected)
         {
-            var firstSut = new QuadKey(first);
-            var secondSut = new QuadKey(second);
+            var sut = QuadKey.From(latitude, longitude, level);
+            Assert.Equal(expected, sut.ToString());
+        }
 
-            Assert.Equal(expected, firstSut.Contains(secondSut));
+        [Theory]
+        [InlineData("120", true, 0b0010_01000011)]
+        [InlineData("3210", true, 0b000110_11000100)]
+        [InlineData("4", false, 0)]
+        [InlineData("222222222222222222222222222222", false, 0b1010101010101010101010101010101010101010101010101010101010011110)]
+        public void try_parse_works_as_expected(string input, bool expectedIsValid, ulong expectedRawValue)
+        {
+            QuadKey.TryParse(input, out var result);
+            Assert.Equal(expectedIsValid, result.IsValid);
+            Assert.Equal(expectedRawValue, result.RawValue);
         }
     }
 }
