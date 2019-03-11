864acf3bea56ee58886596d932ba76de96f7a83f
Simon Lightfoot <simon@devangels.london>
2019-03-11 16:50:20 +0000

    Stop red/yellow ErrorWidget from being displayed on Production builds.

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:inf/app/inf_app.dart';
 import 'package:inf/backend/backend.dart';
 import 'package:inf/utils/error_capture.dart';
@@ -6,6 +7,9 @@ import 'package:flutter/services.dart' show rootBundle;
 
 void main() {
   setupBackend(mode: AppMode.prod, assetLoader: rootBundle.load);
+
+  /// Replace Red/Yellow ErrorWidget with custom one for production releases.
+  ErrorWidget.builder = (FlutterErrorDetails details) => const Center(child: Text('An error occurred'));
 
   /// runCapturedApp ensures that exceptions will be reported to Sentry in release mode
   runCapturedApp(SwayApp(), backend<ErrorReporter>());
