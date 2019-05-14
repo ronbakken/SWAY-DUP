fc40c538350f6e33abbe2eb808852687972636db
Simon Lightfoot <simon@devangels.london>
2019-05-14 23:27:24 +0100

    WIP for Clear and Clear All for filters.

--- lib/ui/filter/filter_confirmation.dart
+++ lib/ui/filter/filter_confirmation.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:inf/app/assets.dart';
 import 'package:inf/app/theme.dart';
 import 'package:inf/ui/widgets/inf_icon.dart';
+import 'package:inf/ui/widgets/widget_utils.dart';
 
 class FilterConfirmButton extends StatefulWidget {
   const FilterConfirmButton({
@@ -40,47 +41,92 @@ class FilterConfirmButtonState extends State<FilterConfirmButton> {
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     return Stack(
-      fit: StackFit.expand,
-      alignment: Alignment.bottomCenter,
       children: <Widget>[
         widget.child,
         Positioned(
+          left: 0.0,
+          right: 0.0,
           bottom: 24.0 + mediaQuery.padding.bottom,
           child: ScaleTransition(
             scale: widget.showHideAnimation,
-            child: RawMaterialButton(
-              onPressed: _delegate.onPressed,
-              fillColor: AppTheme.lightBlue,
-              constraints: const BoxConstraints(minWidth: 64.0, minHeight: 64.0),
-              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-              shape: const CircleBorder(),
-              padding: const EdgeInsets.only(top: 4.0),
-              child: Container(
-                width: 64.0,
-                height: 64.0,
-                alignment: Alignment.center,
-                child: AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 300),
-                  transitionBuilder: (Widget child, Animation<double> animation) {
-                    return ScaleTransition(
-                      scale: animation,
-                      child: child,
-                    );
-                  },
-                  child: InfIcon(_delegate.icon, size: 28.0, key: ValueKey(_delegate.icon)),
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Offstage(
+                  offstage: true,
+                  child: FractionallySizedBox(
+                    widthFactor: 0.7,
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      height: 64.0,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Expanded(
+                            child: RawMaterialButton(
+                              onPressed: _onClearPressed,
+                              fillColor: AppTheme.blackTwo,
+                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              shape: const StadiumBorder(),
+                              padding: const EdgeInsets.only(right: 16.0),
+                              child: const Text('Clear'),
+                            ),
+                          ),
+                          horizontalMargin16,
+                          Expanded(
+                            child: RawMaterialButton(
+                              onPressed: _onClearAllPressed,
+                              fillColor: AppTheme.blackTwo,
+                              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                              shape: const StadiumBorder(),
+                              padding: const EdgeInsets.only(left: 16.0),
+                              child: const Text('Clear all'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                RawMaterialButton(
+                  onPressed: _delegate.onFabPressed,
+                  fillColor: AppTheme.lightBlue,
+                  constraints: const BoxConstraints(minWidth: 64.0, minHeight: 64.0),
+                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: Container(
+                    width: 64.0,
+                    height: 64.0,
+                    alignment: Alignment.center,
+                    child: AnimatedSwitcher(
+                      duration: const Duration(milliseconds: 300),
+                      transitionBuilder: (Widget child, Animation<double> animation) {
+                        return ScaleTransition(
+                          scale: animation,
+                          child: child,
+                        );
+                      },
+                      child: InfIcon(_delegate.icon, size: 28.0, key: ValueKey(_delegate.icon)),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
       ],
     );
   }
+
+  void _onClearPressed() {}
+
+  void _onClearAllPressed() {}
 }
 
 class FilterConfirmButtonDelegate {
   final AppAsset icon;
-  final VoidCallback onPressed;
+  final VoidCallback onFabPressed;
 
-  const FilterConfirmButtonDelegate(this.icon, [this.onPressed]);
+  const FilterConfirmButtonDelegate(this.icon, [this.onFabPressed]);
 }
