d7f3f01e2ce0133afb98697c9a45831f35ba8fbf
Simon Lightfoot <simon@devangels.london>
2019-05-19 05:40:54 +0100

    Website home page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,331 @@
+import 'dart:math' as math;
+
 import 'package:flutter_web/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3, radians;
 
 void main() => runApp(MyApp());
 
+final carouselItems = <_CarouselModel>[
+  _CarouselModel('food', 'Food'),
+  _CarouselModel('drinks', 'Drinks'),
+  _CarouselModel('fashion', 'Fashion'),
+  _CarouselModel('heart', 'Heart'),
+  _CarouselModel('beauty', 'Beauty'),
+  _CarouselModel('plane', 'Travel'),
+  _CarouselModel('drinks', 'Coffee'),
+];
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sway Marketplace',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primaryColor: Color(0xFFD7BFFD),
+        fontFamily: 'ProximaNova',
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    final phoneAlignment = const Alignment(0.125, 0.1);
+    return Material(
+      child: OverflowBox(
+        minWidth: 720,
+        minHeight: 480,
+        child: Stack(
+          fit: StackFit.expand,
           children: <Widget>[
-            Text(
-              'Hello, World!',
+            _Background(),
+            _Carousel(
+              models: carouselItems,
+              front: false,
+              alignment: phoneAlignment,
+            ),
+            Align(
+              alignment: phoneAlignment,
+              child: _Phone(),
+            ),
+            Align(
+              alignment: const Alignment(-0.75, 0.6),
+              child: SizedBox(
+                width: 340.0,
+                child: _HomeContent(),
+              ),
+            ),
+            _Carousel(
+              models: carouselItems,
+              front: true,
+              alignment: phoneAlignment,
             ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class _Carousel extends StatefulWidget {
+  const _Carousel({
+    Key key,
+    @required this.models,
+    @required this.front,
+    this.alignment = Alignment.center,
+  }) : super(key: key);
+
+  final List<_CarouselModel> models;
+  final bool front;
+  final Alignment alignment;
+
+  @override
+  _CarouselState createState() => _CarouselState();
+}
+
+class _CarouselState extends State<_Carousel> {
+  final _duration = ValueNotifier<Duration>(Duration.zero);
+  Ticker _ticker;
+
+  @override
+  void initState() {
+    super.initState();
+    _ticker = Ticker(_onTick, debugLabel: 'carousel');
+    _ticker.start();
+  }
+
+  void _onTick(Duration elapsed) {
+    _duration.value = elapsed;
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: Flow(
+        delegate: _CarouselFlow(
+          duration: _duration,
+          front: widget.front,
+          alignment: widget.alignment,
+        ),
+        children: widget.models.map((item) {
+          return _CarouselItem(model: item);
+        }).toList(),
+      ),
+    );
+  }
+}
+
+class _CarouselFlow extends FlowDelegate {
+  _CarouselFlow({
+    @required this.duration,
+    @required this.front,
+    @required this.alignment,
+  }) : super(repaint: duration);
+
+  final ValueNotifier<Duration> duration;
+  final bool front;
+  final Alignment alignment;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    final tilt = 87.0;
+    final size = context.size;
+    final aligned = alignment.alongSize(size);
+    final count = context.childCount;
+    for (int index = 0; index < count; index++) {
+      final result = Matrix4.identity();
+      result.translate(aligned.dx, aligned.dy);
+      result.multiply(Matrix4.identity()..rotateX(radians(tilt)));
+
+      double time = duration.value.inMicroseconds / Duration.microsecondsPerSecond;
+      double angle = (360.0 * (time / 40)) + (index * (360.0 / count)) % 360;
+
+      final x = math.sin(radians(angle)) * size.width * 0.35;
+      final y = math.cos(radians(angle)) * size.height * 2.0;
+      final pos = result.transform3(Vector3(x, y, 1.0));
+      final scale = ((pos.z + 2000) * 0.0005).clamp(0.6, 1.25);
+
+      result.multiply(Matrix4.identity()..translate(x, y));
+      result.multiply(Matrix4.diagonal3Values(scale, scale, scale));
+      result.multiply(Matrix4.identity()..rotateX(radians(-tilt)));
+
+      if (angle < 90 || angle > 270) {
+        if (front) context.paintChild(index, transform: result);
+      } else {
+        if (!front) context.paintChild(index, transform: result);
+      }
+    }
+  }
+
+  @override
+  BoxConstraints getConstraintsForChild(int i, BoxConstraints constraints) {
+    return constraints; // BoxConstraints(maxWidth: 48.0, maxHeight: 48.0);
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) => false;
+}
+
+class _CarouselItem extends StatelessWidget {
+  const _CarouselItem({
+    Key key,
+    @required this.model,
+  }) : super(key: key);
+
+  final _CarouselModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: 48.0,
+          height: 48.0,
+          decoration: BoxDecoration(
+            color: theme.primaryColor.withOpacity(0.7),
+            border: Border.all(color: Colors.white, width: 1.8),
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(8.0),
+          child: Image.asset(
+            'icons/${model.asset}.png',
+            semanticLabel: model.name,
+          ),
+        ),
+        Positioned(
+          top: 48.0,
+          left: 22.5,
+          width: 1.0,
+          child: Container(
+            width: 1.0,
+            height: 96.0,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[
+                  Colors.white,
+                  Colors.white.withOpacity(0.0),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _CarouselModel {
+  const _CarouselModel(this.asset, this.name);
+
+  final String asset;
+  final String name;
+}
+
+class _HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Align(
+          alignment: Alignment.centerRight,
+          widthFactor: 0.96,
+          child: Image.asset('logo.png', width: 240),
+        ),
+        const SizedBox(height: 12.0),
+        Text(
+          'Find and be found',
+          style: theme.textTheme.headline.copyWith(
+            fontSize: 32.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const SizedBox(height: 12.0),
+        Text(
+          'We have created a new way for businesses and '
+          'content creators to interact and do business.',
+          style: TextStyle(
+            fontSize: 16.0,
+            height: 1.8,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Background extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Image.asset('background.jpg', fit: BoxFit.cover),
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[
+                Colors.transparent,
+                Colors.black.withOpacity(0.6),
+                Colors.black,
+              ],
+              stops: <double>[
+                0.5,
+                0.75,
+                1.0,
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Phone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return FittedBox(
+      fit: BoxFit.scaleDown,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            width: 210,
+            height: 434,
+            decoration: BoxDecoration(
+              color: theme.primaryColor.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(28.0),
+              backgroundBlendMode: BlendMode.modulate,
+            ),
+          ),
+          RepaintBoundary(
+            child: Image.asset(
+              'phone.png',
+              width: 220,
+              height: 440,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
