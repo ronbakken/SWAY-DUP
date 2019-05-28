750409b87047faec8783d8b7292c90cb8903341e
Simon Lightfoot <simon@devangels.london>
2019-05-28 21:45:58 +0100

    Updated Sentry DSN key to inf-alpha project.

--- lib/backend/api_keys.dart
+++ lib/backend/api_keys.dart
@@ -1,4 +1,4 @@
 class ApiKeys {
   static String sentry =
-      'https://098cda8db3b94f1a83261e048aa7e65d:1b447ee24d8b4051a9fd0237d6e9f2bd@sentry.io/1311247';
+      'https://15940735e47840f1969ea1eaf176e9df:a7c65de82059407a94db271709ceedbc@sentry.io/1470069';
 }
