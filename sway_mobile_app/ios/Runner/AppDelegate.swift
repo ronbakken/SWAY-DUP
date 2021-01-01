0e025ee45ae83075db5290d71278ebca8780ffaa
Jan Boon <jan.boon@kaetemi.be>
2021-01-02 06:55:03 +0800

    Disable app center integration for now

--- sway_mobile_app/ios/Runner/AppDelegate.swift
+++ sway_mobile_app/ios/Runner/AppDelegate.swift
@@ -1,9 +1,9 @@
 import UIKit
 import Flutter
 // import Firebase
-import AppCenter
-import AppCenterAnalytics
-import AppCenterCrashes
+// import AppCenter
+// import AppCenterAnalytics
+// import AppCenterCrashes
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -14,10 +14,10 @@ import AppCenterCrashes
     GeneratedPluginRegistrant.register(with: self)
     // FirebaseApp.configure()
     // INF Sandbox App Center API key
-    MSAppCenter.start("c011adf3-8607-41bf-855a-4476b212ceb4", withServices:[
-      MSAnalytics.self,
-      MSCrashes.self
-    ])
+    // MSAppCenter.start("c011adf3-8607-41bf-855a-4476b212ceb4", withServices:[
+    //   MSAnalytics.self,
+    //   MSCrashes.self
+    // ])
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
