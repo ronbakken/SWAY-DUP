f05838a7c4fbbbecd7ed4bb08bd83745bd896299
Jan Boon <jan.boon@kaetemi.be>
2021-01-02 03:19:31 +0800

    Update AppCenter

--- sway_mobile_app/android/app/src/main/kotlin/com/example/sway_mobile_app/MainActivity.kt
+++ sway_mobile_app/android/app/src/main/kotlin/com/example/sway_mobile_app/MainActivity.kt
@@ -2,6 +2,13 @@ package app.infsandbox
 
 import io.flutter.embedding.android.FlutterActivity
 
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 class MainActivity: FlutterActivity() {
-	// AppCenter.start(getApplication(), "17c4d591-44f7-40f0-aabd-d36a1e4929cf", Analytics.class, Crashes.class);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+		AppCenter.start(getApplication(), "17c4d591-44f7-40f0-aabd-d36a1e4929cf", Analytics.class.java, Crashes.class.java);
+    }
 }
